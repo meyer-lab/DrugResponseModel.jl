ff44933206ed9075b9a1a3868a54a26f7c1f1e0a
Farnazm <34812750+Farnazmdi@users.noreply.github.com>
2019-06-11 19:16:33 -0700

    Update DDEmodel.jl

--- model/DDEmodel.jl
+++ model/DDEmodel.jl
@@ -1,12 +1,12 @@
 import CSV
-data = CSV.read("C:\\Users\\farna\\Desktop\\Gem.csv")
+data = CSV.read("Gem.csv")
 G2 = data[:,3]
 
 using Optim, LeastSquaresOptim, DelayDiffEq
 using DifferentialEquations, OrdinaryDiffEq
 
-tau1 = 10.0
-tau2 = 20.0
+tau1 = 5.0
+tau2 = 5.0
 # p = [alpha, beta, gamma1, gamma2]
 
 #function G1_G2(du, u, h, p, t)
@@ -23,11 +23,11 @@ end
 
 
 lags = [tau1, tau2]
-h(p, t) = ones(2)
+h(p, t) = 2*ones(2)
 t = LinRange(0.0, 95.5, 192)
-p = [1.01, 1.0, 0.1, 0.01]
+p = [3.1, 0.01, 2.71, 0.01]
 tspan = (0.0, 95.5)
-u0 = [1.0, 1.0]
+u0 = [10.0, 10.0]
 
 prob = DDEProblem(G1_G2, u0, h, tspan, p; constant_lags = lags)
 alg = MethodOfSteps(Tsit5())
