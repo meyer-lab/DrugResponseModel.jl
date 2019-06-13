15a5f04eab7120b7381f22f212afd0531d4e037c
farnazmdi <farnazmd95@gmail.com>
2019-06-12 18:06:17 -0700

    updated

--- model/DDEmodel.jl
+++ model/DDEmodel.jl
@@ -1,40 +1,73 @@
 import CSV
+using LeastSquaresOptim, DifferentialEquations, DelayDiffEq, DiffEqBase
+
 data = CSV.read("Gem.csv")
-G2 = data[:,3]
+total = CSV.read("Gem_pop.csv")
 
-using Optim, LeastSquaresOptim, DelayDiffEq
-using DifferentialEquations, OrdinaryDiffEq
+# we could choose the columns of "total" and "data" from [2:8]
+total_old = total[:,5]
+G2_old = data[:,5]
 
-tau1 = 5.0
-tau2 = 5.0
-# p = [alpha, beta, gamma1, gamma2]
+estim_init = [4.25, 2.0] # [init_g1, init_g2] 
+# rescaling the experimental data
+total_new = (estim_init[1] + estim_init[2]) * total_old
+G2_new = 0.01*total_new.*G2_old
+G1_new = total_new - G2_new
 
-#function G1_G2(du, u, h, p, t)
-# This model assumes delay for dying
-#    du[1] = -p[1]*(h(p, t-tau1)[1]) + 2*p[2]*(h(p, t-tau2)[2]) - p[3]*(h(p, t-tau1)[1])
-#    du[2] = p[1]*(h(p, t-tau1)[1]) - p[2]*(h(p, t-tau2)[2]) - p[4]*(h(p, t-tau2)[2])
-#end
 
 # This model doesn't assume delay for dying
 function G1_G2(du, u, h, p, t)
-    du[1] = -p[1]*(h(p, t-tau1)[1]) + 2*p[2]*(h(p, t-tau2)[2]) - p[3]*u[1]
-    du[2] = p[1]*(h(p, t-tau1)[1]) - p[2]*(h(p, t-tau2)[2]) - p[4]*u[2]
+    du[1] = -p[1]*(h(p, t-p[5])[1]) + 2*p[2]*(h(p, t-p[6])[2]) - p[3]*u[1]
+    du[2] = p[1]*(h(p, t-p[5])[1]) - p[2]*(h(p, t-p[6])[2]) - p[4]*u[2] 
 end
 
+p = [1.0045,-0.418798,7.47465,0.3212957,19.17196,16.88276,4.2931205,1.96889172]
 
-lags = [tau1, tau2]
+function resid(pp)
+    lags = [pp[5], pp[6]]
+    h(p, t) = 2*ones(2)
+    t = LinRange(0.0, 95.5, 192)
+    
+    tspan = (0.0, 95.5)
+    u0 = [pp[7], pp[8]]
+    prob = DDEProblem(G1_G2, [pp[7], pp[8]], h, tspan, pp; constant_lags = lags)
+
+    sol = solve(prob, MethodOfSteps(Tsit5()))
+    
+    res = zeros(2, length(t))
+    res[1,:] = sol(t, idxs=1).u - G1_new
+    res[2,:] = sol(t, idxs=2).u - G2_new
+    return res
+end
+
+results = optimize(resid, p, Dogleg())
+
+
+## ---------------------- solve given the estimated parameters ----------------------------##
+
+params = results.minimizer
+
+# updating the parameters to solve DDE
+lags = [params[5], params[6]]
+u0 = [params[7], params[8]]
+
+total_new = (p[7] + p[8]) * total_old
+G2_new = 0.01*total_new.*G2_old
+G1_new = total_new - G2_new
+
+tspan = (0.0, 95.5)
 h(p, t) = 2*ones(2)
 t = LinRange(0.0, 95.5, 192)
-p = [3.1, 0.01, 2.71, 0.01]
-tspan = (0.0, 95.5)
-u0 = [10.0, 10.0]
 
-prob = DDEProblem(G1_G2, u0, h, tspan, p; constant_lags = lags)
-alg = MethodOfSteps(Tsit5())
-# sol = solve(prob, alg)
-# using Plots; plot(sol)
-cost = build_loss_objective(prob,t,G2,MethodOfSteps(Tsit5()),maxiters=10000)
-results = optimize(cost, u0, Dogleg()) # this is the problematic sentence
+# solve
+estim_prob = DDEProblem(G1_G2, u0, h, tspan, params; constant_lags = lags)
+sol = solve(estim_prob, MethodOfSteps(Tsit5()))
+total_cells = sol(t, idxs=1).u + sol(t, idxs=2).u
 
+# plot
+using Plots; 
 
-
+plot(t, sol(t, idxs=2).u, label = "est G2")
+plot!(t, G2_new, label = "G2")
+plot!(t, total_cells, label = "est total")
+plot!(t, total_new, label = "total")
