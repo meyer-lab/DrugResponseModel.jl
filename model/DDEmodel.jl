349ab1c88260fa4e709b8f0d3eb5ef1ec9e1a847
Farnazm <34812750+Farnazmdi@users.noreply.github.com>
2019-06-17 11:13:43 -0700

    samin() added

--- model/DDEmodel.jl
+++ model/DDEmodel.jl
@@ -1,17 +1,17 @@
 import CSV
-data = CSV.read("Gem.csv")
-total = CSV.read("Gem_pop.csv")
+using LeastSquaresOptim, DifferentialEquations, DelayDiffEq, DiffEqBase, Optim, LinearAlgebra
+
+data = CSV.read("DOX.csv")
+total = CSV.read("DOX_pop.csv")
 
 total_old = total[:,7]
 G2_old = data[:,7]
 
-estim_init = [4.25, 2.0] # [init_g1, init_g2]
+estim_init = [3.0, 3.4] # [init_g1, init_g2]
 # rescaling the experimental data
 total_new = (estim_init[1] + estim_init[2]) * total_old
 G2_new = 0.01*total_new.*G2_old
 G1_new = total_new - G2_new
-
-using LeastSquaresOptim, DifferentialEquations, DelayDiffEq, DiffEqBase, Optim
 
 
 # This model doesn't assume delay for dying
@@ -19,8 +19,6 @@ function G1_G2(du, u, h, p, t)
     du[1] = -p[1]*(h(p, t-p[5])[1]) + 2*p[2]*(h(p, t-p[6])[2]) - p[3]*u[1]
     du[2] = p[1]*(h(p, t-p[5])[1]) - p[2]*(h(p, t-p[6])[2]) - p[4]*u[2] 
 end
-
-p = [1.0045, 0.42, 0.01, 0.01, 19.17196, 16.88276, 4.2931205, 1.96889172, 3.0]
 
 function solution(pp)
     lags = [pp[5], pp[6]]
@@ -30,45 +28,109 @@ function solution(pp)
     tspan = (0.0, 95.5)
     u0 = [pp[7], pp[8]]
     prob = DDEProblem(G1_G2, [pp[7], pp[8]], h, tspan, pp; constant_lags = lags)
-    solve(prob, MethodOfSteps(Tsit5()), lower = [0.0, 0.0], upper = [Inf, Inf])
+    solve(prob, MethodOfSteps(Tsit5()), lower = [0.0, 0.0])
 end
 
 function resid(pp)
     t = LinRange(0.0, 95.5, 192)
     res = zeros(2, length(t))
     sol = solution(pp)
-    res[1,:] = sol(t, idxs=1).u - G1_new
-    res[2,:] = sol(t, idxs=2).u - G2_new
+    res[1,:] = sol(t, idxs=1) - G1_new
+    res[2,:] = sol(t, idxs=2) - G2_new
     return res
 end
 
+p = [1.078,0.869,0.7141,0.2419,20.0988,20.677,2.834,2.719, 3.0]
 lowwer = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-results = optimize(resid, p, Dogleg(), lower = lowwer)
+upp = [3.0, 3.0, 3.0, 3.0, 30.0, 30.0, 10.0, 10.0, 10.0]
+results = optimize(resid, p, Dogleg(), lower = lowwer, upper = upp)
 
 ## ------------------ Solve with new params ----------------------- ##
 
-function ReSolve(results)
-    t = LinRange(0.0, 95.5, 192)
-    params = results.minimizer
-    lags = [params[5], params[6]]
-    u0 = [params[7], params[8]]
+params = results.minimizer
+total_new = (params[7] + params[8]) * total_old
+G2_new = 0.01*total_new.*G2_old
+G1_new = total_new - G2_new
 
-    tspan = (0.0, 95.5)
-    h(p, t) = params[9]*ones(2)
-    t = LinRange(0.0, 95.5, 192)
-    total_new = (params[7] + params[8]) * total_old
-    G2_new = 0.01*total_new.*G2_old
-    G1_new = total_new - G2_new
-
-    estim_prob = DDEProblem(G1_G2, u0, h, tspan, params; constant_lags = lags)
-    sol = solve(estim_prob, MethodOfSteps(Tsit5()), lower = [0.0, 0.0], upper = [Inf, Inf])
-end
-
-sol = ReSolve(results)
+sol = solution(params)
 t = LinRange(0.0, 95.5, 192)
 
 using Plots; 
-plot(t, sol(t, idxs=2).u, label = "est G2", title = "Trial 7")
-plot!(t, (sol(t, idxs=2).u + sol(t, idxs=1).u), label = "est total")
-plot!(t, sol(t, idxs=1).u, label = "G1 est")
-savefig("7.png")
+plot(t, sol(t, idxs=2).u, label = "G2 est", title = "Dox Trial 7 Dogleg()")
+plot!(t, G2_new, label = "G2 new")
+plot!(t, (sol(t, idxs=2).u + sol(t, idxs=1).u), label = "total est")
+plot!(t, total_new, label = "total new")
+savefig("DOX7_Dogleg.png")
+
+
+
+
+
+
+## ====================== Use Simulated Annealing =========================##
+
+import CSV
+using LeastSquaresOptim, DifferentialEquations, DelayDiffEq
+using DiffEqBase, Optim, LinearAlgebra, Plots
+data = CSV.read("DOX.csv")
+total = CSV.read("DOX_pop.csv")
+
+total_old = total[:,7]
+G2_old = data[:,7]
+
+estim_init = [1.88, 3.25] # [init_g1, init_g2]
+# rescaling the experimental data
+total_new = (estim_init[1] + estim_init[2]) * total_old
+G2_new = 0.01*total_new.*G2_old
+G1_new = total_new - G2_new
+ 
+# This model doesn't assume delay for dying
+function G1_G2(du, u, h, p, t)
+    du[1] = -p[1]*(h(p, t-p[5])[1]) + 2*p[2]*(h(p, t-p[6])[2]) - p[3]*u[1]
+    du[2] = p[1]*(h(p, t-p[5])[1]) - p[2]*(h(p, t-p[6])[2]) - p[4]*u[2] 
+end
+
+function solution(pp)
+    lags = [pp[5], pp[6]]
+    h(p, t) = pp[9]*ones(2)
+    t = LinRange(0.0, 95.5, 192)
+    tspan = (0.0, 95.5)
+    u0 = [pp[7], pp[8]]
+
+    prob = DDEProblem(G1_G2, [pp[7], pp[8]], h, tspan, pp; constant_lags = lags)
+    sol = solve(prob, MethodOfSteps(Tsit5()), lower = [0.0, 0.0], upper = [20.0, 20.0])
+end
+
+function resid(pp)
+    t = LinRange(0.0, 95.5, 192)
+    sol = solution(pp)
+    res = sol(t, idxs=2) - G2_new
+    
+#     res = zeros(2, length(t))
+#     res[1,:] = sol(t, idxs=1) - G1_new
+#     res[2,:] = sol(t, idxs=2) - G2_new
+    return norm(res)
+end
+
+p = [0.470,0.455,0.490,0.021,17.613,17.626,2.978,2.773,2.923]
+lowwer = [0.0, 0.0, 0.0, 0.0, 5.0, 5.0, 0.0, 0.0, 0.0]
+upperr = [100.0, 100.0, 100.0, 80.0, 40.0, 40.0, 20.0, 20.0, 100.0]
+
+res = Optim.optimize(resid, lowwer, upperr, p, SAMIN(rt = 0.7), Optim.Options(iterations = 10^8))
+
+
+# ---------------- Solve with new parameters from samin() ----------------------#
+
+total_nw = (res.minimizer[7] + res.minimizer[8]) * total_old
+G2_new = 0.01*total_nw.*G2_old
+G1_new = total_nw - G2_new
+t = LinRange(0.0, 95.5, 192)
+
+final = solution(res.minimizer)
+
+plot(t, final(t, idxs=2).u, label = "G2 est", title = "Dox Trial 7 samin(rt=0.7)")
+plot!(t, G2_new, label = "G2")
+plot!(t, (final(t, idxs=2).u + final(t, idxs=1).u), label = "total est")
+plot!(t, total_nw, label = "total")
+# plot!(t, final(t, idxs=1).u, label = "est G1")
+savefig("DOX_one_7_samin_rt7.png")
