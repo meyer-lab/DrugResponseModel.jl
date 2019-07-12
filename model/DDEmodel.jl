6db3fd0914c4ed5d6a52b8f715fc93972c68c273
farnazmdi <farnazmd95@gmail.com>
2019-07-12 13:46:05 -0700

    notebook and DDEmodel.jl update

--- model/DDEmodel.jl
+++ model/DDEmodel.jl
@@ -1,17 +1,12 @@
 using LeastSquaresOptim, DifferentialEquations, DelayDiffEq, DiffEqBase, Optim, Plots, Statistics, DataFrames, CSV, Distributed
 
-@everywhere begin
-    """This file contains functions to:
+"""
+        This file contains functions to:
     1. Import data, extracting G1 and G2 data in # of cells.  
     2. Fit the data to a Delay Differential Equation model, and find the parameters.
-    """
-    function foo()
-        nothing
-    end
-end
+"""
 
-
-function get_data(path_g2, path_total)
+function get_data(path_g2::String, path_total::String)
     # Import data all the trials for each drug
     data = CSV.read(path_g2)
     total = CSV.read(path_total)
@@ -21,14 +16,15 @@ function get_data(path_g2, path_total)
     deletecols!(total, 1:2)
 
     # getting all the 8 trials
-    drug = data[:, 1:8]
-    pop = total[:, 1:8]
+    drug = data[1:192, 1:8]
+    pop = total[1:192, 1:8]
 
     # rescaling the experimental data assuming we have 20 initial cells for each trial
     g1 = zeros(size(drug, 1), 8)
     g2 = zeros(size(drug, 1), 8)
     g1_0 = zeros(8)
     g2_0 = zeros(8)
+    initial_val = fill([], 8)
 
     init_cells = 20.0
 
@@ -39,8 +35,10 @@ function get_data(path_g2, path_total)
         g1[:, i] = pop[:, i] .- g2[:, i]
         g2_0[i] = init_cells*(drug[1, i]/100.0)
         g1_0[i] = init_cells*(1 - drug[1, i]/100.0)
+        initial_val[i] = [g1_0[i], g2_0[i]]
     end
-    return pop, g2, g1, g2_0, g1_0
+
+    return pop, g2, g1, initial_val
 end
 
 # Specifying which trial of the data is being used; could be an integer belonging to {1, 3, ..., 8}
@@ -51,27 +49,28 @@ function DDEmodel(du, u, h, p, t)
     du[2] = p[1]*(h(p, t-p[3])[1]) - p[2]*(h(p, t-p[4])[2]) - p[7]*u[2]
 end
 
-function DDEsolve(pp)
+function DDEsolve(pp, i)
     lags = [pp[3], pp[4]]
-    h(p, t) = pp[5]*ones(2)
+    h(pp, t) = pp[5]*ones(2)
     t = LinRange(0.0, 95.5, 192)
     tspan = (0.0, 95.5)
-    u0 = [gem1_0[i], gem2_0[i]]
+    u0 = initial_val[i]
     prob = DDEProblem(DDEmodel, u0, h, tspan, pp; constant_lags = lags)
-    solve(prob)
+    sol = solve(prob, MethodOfSteps(Tsit5()))
+    return sol
 end
 
 function resid(pp)
     t = LinRange(0.0, 95.5, 192)
     res = zeros(2, 192)
-    sol = DDEsolve(pp)
-    res[1, :] = sol(t, idxs=1).u - gem1[:, i]
-    res[2, :] = sol(t, idxs=2).u - gem2[:, i]
+    sol = DDEsolve(pp, i)
+    res[1, :] = sol(t, idxs=1).u - g1[:, i]
+    res[2, :] = sol(t, idxs=2).u - g2[:, i]
     return res
 end
 
-function optimIt(p, low, upp)
-    results_dde = optimize(resid, p, LevenbergMarquardt(), lower = low, upper = upp)
+function optimIt(initial_guess, lower_bound, upper_bound)
+    results_dde = optimize(resid, initial_guess, LevenbergMarquardt(), lower = lower_bound, upper = upper_bound)
     return results_dde.minimizer
 end
 
