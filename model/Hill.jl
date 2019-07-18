811c5777e3c44969486e31d47ad60316815ae9b1
farnazmdi <farnazmd95@gmail.com>
2019-07-17 22:00:03 -0700

    some changes to h(p, t)

--- model/Hill.jl
+++ model/Hill.jl
@@ -1,27 +1,33 @@
+""" This file contains Hill function, residuals of Hill based off of DDE, and optimization of it."""
 include("DDEmodel.jl")
 
-hill(p, concentration) =  p[1] + ((p[2] - p[1]) / (1 + 10^((concentration - p[3])*p[4])))
+# hillParams should be something like [EC50, alpha_min, alpha_max, alpha_b, EC50, beta_min, beta_max, beta_b...]
+hill(p, concentration) =  p[2] + ((p[3] - p[2]) / (1 + 10^((concentration - p[1])*p[4])))
 
 function residHill(hillParams, concentrations, g1, g2)
-    
-    concentration_size = length(concentrations)
-    data_size = length(g1[:, 1])
-    total = zeros(2, data_size*concentration_size)
-    for ii in 1:concentration_size
-        alpha = hill(hillParams[1:4], concentrations[ii])
-        beta = hill(hillParams[5:8], concentrations[ii])
-        tau1 = hill(hillParams[9:12], concentrations[ii])
-        tau2 = hill(hillParams[13:16], concentrations[ii])
-        history = hill(hillParams[17:20], concentrations[ii])
-        gamma1 = hill(hillParams[21:24], concentrations[ii])
-        gamma2 = hill(hillParams[25:28], concentrations[ii])
+    """ This functions takes in hill parameters for all the concentrations and calculates
+    DDE parameters, passes them to residual function and based off of these, optimizes the model
+    and estimates hill parameters. """
 
-        pp = [alpha, beta, tau1, tau2, history, gamma1, gamma2]
+    num_concentration = length(concentrations)
+    data_size = length(g1[:, 1])
+    total_res = zeros(2, data_size*num_concentration)
+
+#     EC50  for all of the trials is hillParams[1]
+    for ii in 1:num_concentration
+        alpha = hill(append!([hillParams[1]], hillParams[2:4]), concentrations[ii])
+        beta = hill(append!([hillParams[1]], hillParams[5:7]), concentrations[ii])
+        tau1 = hill(append!([hillParams[1]], hillParams[8:10]), concentrations[ii])
+        tau2 = hill(append!([hillParams[1]], hillParams[11:13]), concentrations[ii])
+        gamma1 = hill(append!([hillParams[1]], hillParams[14:16]), concentrations[ii])
+        gamma2 = hill(append!([hillParams[1]], hillParams[17:19]), concentrations[ii])
+
+        pp = [alpha, beta, tau1, tau2, gamma1, gamma2]
         residuals = resid(pp, ii, g1, g2)
-        total[1, ((ii-1)*data_size +1):(ii*data_size)] = residuals[1, :]
-        total[2, ((ii-1)*data_size +1):(ii*data_size)] = residuals[2, :]
+        total_res[1, ((ii-1)*data_size +1):(ii*data_size)] = residuals[1, :]
+        total_res[2, ((ii-1)*data_size +1):(ii*data_size)] = residuals[2, :]
     end 
-    return total
+    return total_res
 end
 
 
