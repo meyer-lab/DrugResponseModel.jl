aa0ba14be14c863870ceb37bce2b8b8b87cc3b69
farnazmdi <farnazmd95@gmail.com>
2019-07-20 14:26:14 -0700

    start with the module

--- DrugResponseModel/src/one_state_model.jl
+++
@@ -1,9 +0,0 @@
-module one_state_model
-
-export get_data, optimIt, plotIt, ode_optimIt, ode_plotIt
-
-include("importData.jl")
-include("DDEmodel.jl")
-include("plot.jl")
-
-end
--- README.md
+++
@@ -1,2 +0,0 @@
-# one_state_model
-This is to show oscillations in the number of cells in G1 and in G2 phase of cell cycle.
