f652264b415581fa1975a59156e099e648f02915
Farnazm <34812750+Farnazmdi@users.noreply.github.com>
2019-07-24 18:56:01 -0700

    Hill (#7)
    * first attempt for hill
    
    * update notebook
    
    * removed history from params
    
    * removed history from plot
    
    * some changes to h(p, t)
    
    * hill notebook updated
    
    * h removed
    
    * Update Hill.jl
    
    * death rate changed
    
    * added h(p, t) as exponential
    
    * mistakes
    
    * history changed to exp
    
    * Hill improved
    
    * clean up
    
    * history function added
    
    * some adjustment
    
    * plot.jl update
    
    * DDE notebook update
    
    * tried hill for doxorubicin instead of lapatinib

--- model/plot.jl
+++ model/plot.jl
@@ -1,4 +1,5 @@
-using Plots, CSV, Distributed;
+using Plots, CSV, Distributed, DataFrames;
+include("DDEmodel.jl")
 
 """ 
         This file contains a function to plot the parameters against the  drug concentrations, in a 2x2 subplot.
@@ -16,16 +17,17 @@ function plotIt(params::Array, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array,
     """
     lags = [params[3], params[4]]
     t = LinRange(0.0, 95.5, 192)
-    t_new = LinRange(0.0, 195.5, 292)
-    h(params, t_new) = params[5]*ones(2)
+    t_new = LinRange(0.0, 195.0, 292)
     tspan_new = (0.0, 195.5)
+    h_g1_params, h_g2_params = find_history(g1, g2)
+    h(pp, t_new) = [h_g1_params[1]*exp.(t_new*h_g1_params[2]), h_g2_params[1]*exp.(t_new*h_g2_params[2])]
     u0_new = [g1_0[i], g2_0[i]]
     prob_new = DDEProblem(DDEmodel, u0_new, h, tspan_new, params; constant_lags = lags)
     solution = solve(prob_new, MethodOfSteps(Tsit5()))
 
     plot(t_new, solution(t_new, idxs=1).u, label = "G1 est", dpi = 150, title = title, xlabel = "time [hours]", ylabel = "# of cells")
     plot!(t, g1[:, i], label = "G1", dpi = 150)
-    plot!(t_new, solution(t_new, idxs=2).u, label = "G2 est", legend=:topleft, dpi = 150)
+    plot!(t_new, solution(t_new, idxs=2).u, label = "G2 est", legend=:topright, dpi = 150)
     plot!(t, g2[:, i], label = "G2", dpi = 150)
     plot!(t_new, (solution(t_new, idxs=2).u + solution(t_new, idxs=1).u), label = "total est", dpi = 150)
     plot!(t, pop[i], label = "total", dpi = 150)
@@ -33,21 +35,6 @@ function plotIt(params::Array, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array,
 end
 
 #--------------------- Plot parameteres versus drug concentration ------------------------#
-
-# Reading the file containing parameters
-param_lap_dde = CSV.read(".//figures//Lapatinib//params_lap_DDE.csv")
-param_gem_dde = CSV.read(".//figures//Gem//params_gem_DDE.csv")
-param_dox_dde = CSV.read(".//figures//Dox//params_dox_DDE.csv")
-param_taxol1_dde = CSV.read(".//figures//taxol//params_taxol1_DDE.csv")
-param_tax2_dde = CSV.read(".//figures//taxol2//params_tax2_DDE.csv")
-
-# Convert the DataFrame to Matrix for plotting
-lap = convert(Matrix, param_lap_dde[:,3:end])
-gem = convert(Matrix, param_gem_dde[:,3:end])
-dox = convert(Matrix, param_dox_dde[:,3:end])
-tax = convert(Matrix, param_taxol1_dde[:,3:end])
-tax2 = convert(Matrix, param_tax2_dde[:, 3:end])
-
 
 function plot_param_conc(lap, gem, dox, tax, i, param)
     """ This function to plot parameter vs. concentraition.
@@ -62,13 +49,23 @@ function plot_param_conc(lap, gem, dox, tax, i, param)
     Returns a 2x2 plot for four drugs. 
     
     """
-    p1 = plot(gem[8, :], gem[i, :], label = "Gemcitabine", title = param, xlabel = "drug conc. [nM]", ylabel = "param", yticks = 0.0:0.02:0.5)
-    p2 = plot(lap[8, :], lap[i, :], label = "Lapatinib", xlabel = "drug conc. [nM]", ylabel = "param", yticks = 0.0:0.05:0.5)
-    p3 = plot(dox[8, :], dox[i, :], label = "Doxorubicin", xlabel = "drug conc. [nM]", ylabel = "param", yticks = 0.0:0.05:0.5)
-    p4 = plot(tax[8, :], tax[i, :], label = "Paclitaxel", xlabel = "drug conc. [nM]", ylabel = "param", yticks = 0.0:0.1:1.0)
+    p1 = plot(gem[8, :], gem[i, :], label = "Gemcitabine", title = param, xlabel = "drug conc. [nM]", ylabel = "param", yticks = 0.0:round(maximum(gem[i, :])/5 ,digits = 3):maximum(gem[i, :]))
+    p2 = plot(lap[8, :], lap[i, :], label = "Lapatinib", xlabel = "drug conc. [nM]", ylabel = "param", yticks = 0.0:round(maximum(lap[i, :])/5, digits = 3):maximum(lap[i, :]))
+    p3 = plot(dox[8, :], dox[i, :], label = "Doxorubicin", xlabel = "drug conc. [nM]", ylabel = "param", yticks = 0.0:round(maximum(dox[i, :])/5, digits = 3):maximum(dox[i, :]))
+    p4 = plot(tax[8, :], tax[i, :], label = "Paclitaxel", xlabel = "drug conc. [nM]", ylabel = "param", yticks = 0.0:round(maximum(tax[i, :])/5, digits = 3):maximum(tax[i, :]))
     plot(p1, p2, p3, p4, dpi = 100)
 end
 
-# plot_param_conc(lap, gem, dox, tax, 1, "alpha")
-# We can save the figure by using the following line with an appropriate name in the paramthesis.
-# savefig("")
+# using CSV
+
+# # Reading the file containing params
+# param_lap_dde = CSV.read(".//figures//Lapatinib//params_lap_DDE.csv")
+# param_gem_dde = CSV.read(".//figures//Gem//params_gem_DDE.csv")
+# param_dox_dde = CSV.read(".//figures//Dox//params_dox_DDE.csv")
+# param_taxol1_dde = CSV.read(".//figures//taxol//params_taxol1_DDE.csv")
+
+# # Convert the DataFrame to Matrix for plotting
+# lap = convert(Matrix, param_lap_dde[:,2:end])
+# gem = convert(Matrix, param_gem_dde[:,2:end])
+# dox = convert(Matrix, param_dox_dde[:,2:end])
+# tax = convert(Matrix, param_taxol1_dde[:,2:end])
