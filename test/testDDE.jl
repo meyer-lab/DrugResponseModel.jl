ceeb5b39317f3e5982181e4b32c7f618504cc18a
farnazmdi <farnazmd95@gmail.com>
2019-08-15 10:07:00 -0700

    test update

--- test/testDDE.jl
+++ test/testDDE.jl
@@ -12,25 +12,30 @@ pop, g2, g1, g1_0, g2_0 = get_data("..//data//lap.csv", "..//data//lap_pop.csv")
 @test size(pop[:, 1],1) == size(g1[:, 1],1)
 @test size(g1_0, 1) == size(g2_0, 1)
 
+for i in 1:8
+    pop[:, i] = remove_peaks(pop[:, i])
+    g2[:, i] = remove_peaks(g2[:, i])
+    g1[:, i] = remove_peaks(g1[:, i])
+end
+@test size(g1[:, 1],1) == size(g2[:, 1],1)
 
 ##------------------ Simple tests for DDEsolve function -------------------------##
-# i is the number of the column we are using from the data (# of trial)
-i = 6
+# j is the number of the column we are using from the data (# of trial)
+j = 6
 
 # initial guess
-p  = [0.02798, 0.025502, 21.3481, 10.2881, 0.0001, 0.0001]
+initial_guess  = log.([0.02798, 0.025502, 21.3481, 10.2881, 0.0001, 0.0001])
 
-# setting lowest delay for tau1 to be half an hour and for tau2 to be 3 hours.
-low = [0.015, 0.003, 3.0, 7.0, 0.0001, 0.0001]
-upp = [0.075, 0.075, 30.0, 100.0, 0.05, 0.05]
+# bounds 
+lower_bnd = [-6.0, -6.0, 2.0, 2.0, -10.0, -10.0]
+upper_bnd = [0.0, 0.0, 6.0, 6.0, 0.0, 0.0]
 
-# Estimating the parameters for trial i
-params = optimIt(p, low, upp, i, g1, g2)
+# Estimating the parameters for trial j
+params = optimization(g1, g2, g1_0, g2_0, initial_guess, j)
 
 # to test the estimated parameters are still in the range
 @test length(params) == 6
 for i in 1:6
-    @test upp[i] >= params[i] >= low[i]
+    @test upper_bnd[i] >= log.params[i] >= lower_bnd[i]
 end
 
-plotIt(params, g1, g2, g1_0, g2_0, pop, i, "Lapatinib")
