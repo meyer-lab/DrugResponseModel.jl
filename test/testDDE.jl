e2b4edae94ca4d4c5de054ee8652bd7a7900e098
farnazmdi <farnazmd95@gmail.com>
2019-08-16 15:49:28 -0700

    testDDE updated

--- test/testDDE.jl
+++ test/testDDE.jl
@@ -1,8 +1,8 @@
 # FactCheck and Base.Test are two functions to use for testing
 using Test
+using Profile
 using DrugResponseModel
-include("importData.jl")
-include("DDEmodel.jl")
+
 
 ##------------------ Simple tests for get_input function -----------------------##
 pop, g2, g1, g1_0, g2_0 = get_data("..//data//lap.csv", "..//data//lap_pop.csv")
@@ -23,24 +23,23 @@ end
 j = 6
 
 # initial guess
-initial_guess  = log.([0.02798, 0.025502, 21.3481, 10.2881, 0.0001, 0.0001])
+initial_guess  = [0.02798, 0.025502, 21.3481, 10.2881, 0.0001, 0.0001]
 
 # bounds 
 lower_bnd = [-6.0, -6.0, 2.0, 2.0, -10.0, -10.0]
 upper_bnd = [0.0, 0.0, 6.0, 6.0, 0.0, 0.0]
+bound = collect(zip(lower_bnd, upper_bnd))
 
 # Estimating the parameters for trial j
-params = optimization(g1, g2, g1_0, g2_0, initial_guess, j)
+params = optimization(g1, g2, g1_0, g2_0, initial_guess, j, bound)
 
 # to test the estimated parameters are still in the range
 @test size(params) == 6
 for i in 1:6
     @test upper_bnd[i] >= log.params[i] >= lower_bnd[i]
 end
-<<<<<<< HEAD
 
 # profiling to DDEmodel
-@profile optimization(g1, g2, g1_0, g2_0, initial_guess, j)
+@profile optimization(g1, g2, g1_0, g2_0, initial_guess, j, bound)
 Profile.print()
-=======
->>>>>>> f723d87075804b2701bf3b4654ee99f9ee9a57c7
+
