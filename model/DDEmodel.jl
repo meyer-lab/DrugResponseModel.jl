08f5a46ceeb1779a20cfedee12a5e6add61fa96e
Farnazm <34812750+Farnazmdi@users.noreply.github.com>
2019-08-16 13:29:22 -0700

    Update DDEmodel.jl

--- model/DDEmodel.jl
+++ model/DDEmodel.jl
@@ -1,56 +1,83 @@
-using LeastSquaresOptim, DifferentialEquations, DelayDiffEq, DiffEqBase, Optim, Plots, Statistics, DataFrames, CSV, Distributed, LsqFit
-
+using DelayDiffEq, DiffEqParamEstim, Optim, DataFrames, LsqFit, BlackBoxOptim
+using Plots
+gr()
 """
         This file contains functions to fit the data to a Delay Differential Equation model, and find the parameters.
 """
 
+# model
 function DDEmodel(du, u, h, p, t)
     du[1] = -p[1]*(h(p, t-p[3])[1]) + 2*p[2]*(h(p, t-p[4])[2]) - p[5]*u[1]
     du[2] = p[1]*(h(p, t-p[3])[1]) - p[2]*(h(p, t-p[4])[2]) - p[6]*u[2]
 end
 
-@. exp_model(t, p) = p[1]*exp(t*p[2]) # exponential model
+# estimate history function
+exp_model(t, p) = @. p[1]*exp(t*p[2]) # exponential model
 
-function find_history(g1::Matrix, g2::Matrix)
+function find_history(g1, g2)
 
-    time = LinRange(0.0, 95.5, 192) # x
     control_g1 = g1[:, 1] # y1
     control_g2= g2[:, 1] # y2
     p0 = [1.0, 0.5]
+    times = range(0.0; stop = 95.5, length = 192) # x
+    fit_g1 = curve_fit(exp_model, times, control_g1, p0) 
+    fit_g2 = curve_fit(exp_model, times, control_g2, p0) 
 
-    fit_g1 = curve_fit(exp_model, time, control_g1, p0) 
-    fit_g2 = curve_fit(exp_model, time, control_g2, p0) 
-
-    g1_hist = fit_g1.param # history function for G1
-    g2_hist = fit_g2.param # history function for G2
-
-    return g1_hist, g2_hist
+    return coef(fit_g1), coef(fit_g2)
 end
+
+# define problem generator (optimization in log space)
+function prob_generator(prob, p)
+    exp_p = exp.(p)
+    remake(prob; p = exp_p, constant_lags = [exp_p[3], exp_p[4]])
+end
+
+
+function ddesolve(g1, g2, g1_0, g2_0, params, j)
+    times = range(0.0; stop = 95.5, length = 192)
+    data = vcat(g1[:, j]', g2[:, j]')
     
-function DDEsolve(pp::Array, i::Int, g1_0::Array, g2_0::Array)
-    lags = [pp[3], pp[4]]
-    t = LinRange(0.0, 95.5, 192)
+    # history function
+    fit1, fit2 = find_history(g1, g2)
+    h(p, t) = [exp_model(t, fit1); exp_model(t, fit2)]
 
-    h_g1_params, h_g2_params = find_history(g1, g2)
+    # problem
+    prob = DDEProblem(DDEmodel, [g1_0[j], g2_0[j]], h, extrema(times), params;
+                      constant_lags = [params[3], params[4]])
+    # algorithm to solve
+    alg = MethodOfSteps(Vern6(); constrained=true)
+
+    # objective function
+    obj = build_loss_objective(prob, alg, L2Loss(times, data);
+                               prob_generator = prob_generator,
+                               verbose_opt = false)
+
+    # returning estimated parameteres and the objective function
+    return obj(params)
+end
+
+
+function optimization(g1, g2, g1_0, g2_0, initial_guess, j, bound)
+    times = range(0.0; stop = 95.5, length = 192)
+    data = vcat(g1[:, j]', g2[:, j]')
     
-    h(pp, t) = [h_g1_params[1]*exp.(t*h_g1_params[2]), h_g2_params[1]*exp.(t*h_g2_params[2])]
-    tspan = (0.0, 95.5)
-    u0 = [g1_0[i], g2_0[i]]
-    prob = DDEProblem(DDEmodel, u0, h, tspan, pp; constant_lags = lags)
-    solve(prob, MethodOfSteps(Tsit5()))
-end
+    # history function
+    fit1, fit2 = find_history(g1, g2)
+    h(p, t) = [exp_model(t, fit1); exp_model(t, fit2)]
 
-function resid(pp::Array, i::Int, g1::Matrix, g2::Matrix)
-    t = LinRange(0.0, 95.5, 192)
-    res = zeros(2, 192)
-    sol = DDEsolve(pp, i, g1_0, g2_0)
-    res[1, :] = sol(t, idxs=1).u - g1[:, i]
-    res[2, :] = sol(t, idxs=2).u - g2[:, i]
-    return res
-end
+    # problem
+    prob = DDEProblem(DDEmodel, [g1_0[j], g2_0[j]], h, extrema(times), initial_guess;
+                      constant_lags = [initial_guess[3], initial_guess[4]])
+    # algorithm to solve
+    alg = MethodOfSteps(Vern6(); constrained=ture)
 
-function optimIt(initial_guess::Array, lower_bound::Array, upper_bound::Array, i::Int, g1::Matrix, g2::Matrix)
-    residuals(pp) = resid(pp, i, g1, g2)
-    results_dde = optimize(residuals, initial_guess, LevenbergMarquardt(), lower = lower_bound, upper = upper_bound)
-    return results_dde.minimizer
+    # objective function
+    obj = build_loss_objective(prob, alg, L2Loss(times, data);
+                               prob_generator = prob_generator,
+                               verbose_opt = false)
+    # optimizing
+    results_dde = bboptimize(obj; SearchRange=bound,
+                                    NumDimensions = 6, TraceInterval=100, Method =:adaptive_de_rand_1_bin_radiuslimited)
+    # returning estimated parameteres and the objective function
+    return exp.(best_candidate(results_dde))
 end
