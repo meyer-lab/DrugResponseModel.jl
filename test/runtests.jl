89ba8f9472fb88fdf8444c8a621643d989fa1464
farnazmdi <farnazmd95@gmail.com>
2019-09-06 13:27:17 -0700

    wrapping up

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,4 @@ using Test, DrugResponseModel
 println("Starting tests")
 
 include("testDDE.jl")
-# include("testHill.jl")
-# include("testEachDDE.jl")
-# include("testEachHill.jl")
+include("testHill.jl")
--- test/testHill.jl
+++ test/testHill.jl
@@ -4,31 +4,53 @@ using CSV
 using DrugResponseModel
 
 # import G1, G2, population, and concentrations data
-concentrations, pop, g2, g1, g2_0, g1_0 = setup_data("lapatinib")
-
+conc_l, popl, g2l, g1l, g2_0l, g1_0l = setup_data("lapatinib")
+conc_d, popd, g2d, g1d, g2_0d, g1_0d = setup_data("doxorubicin")
+conc_g, popg, g2g, g1g, g2_0g, g1_0g = setup_data("gemcitabine")
+conc_t, popt, g2t, g1t, g2_0t, g1_0t = setup_data("paclitaxel")
 # make sure there are 8 concentrations
-@test length(concentrations) == 8
+@test length(conc_l) == length(conc_d) == length(conc_g) == length(conc_t) == 8
 
 # testing the BlackBoxOptim 
 
 # guess
 guess = [100.0, 1.0 , 0.007, 0.01, 0.05, 0.045, 8.0286, 30.1100, 12.0, 8.0, 0.0035, 0.04]
 # max number of iterations 
-num_steps=100
+num_steps=1e4
+
 # do the optimization
-_, pt = optimize_hill(guess, concentrations, g1, g2, g1_0, g2_0, num_steps)
+best_fitL, pt_l = optimize_hill(guess, conc_l, g1l, g2l, g1_0l, g2_0l, num_steps)
+best_fitD, pt_d = optimize_hill(guess, conc_d, g1d, g2d, g1_0d, g2_0d, num_steps)
+best_fitG, pt_g = optimize_hill(guess, conc_g, g1g, g2g, g1_0g, g2_0g, num_steps)
+best_fitT, pt_t = optimize_hill(guess, conc_t, g1t, g2t, g1_0t, g2_0t, num_steps)
 
 # check all the parameters to be positive
-@test all(x -> x>0, pt)
-@test length(pt) == 12
+@test all(x -> x>0, pt_l)
+@test length(pt_l) == 12
+@test all(x -> x>0, pt_d)
+@test length(pt_d) == 12
+@test all(x -> x>0, pt_g)
+@test length(pt_g) == 12
+@test all(x -> x>0, pt_t)
+@test length(pt_t) == 12
 
 # make sure all the dde parameters are positive
-dde_params = getDDEparams(pt, concentrations)
+dde_paramsL = getDDEparams(pt_l, conc_l)
+dde_paramsD = getDDEparams(pt_d, conc_d)
+dde_paramsG = getDDEparams(pt_g, conc_g)
+dde_paramsT = getDDEparams(pt_t, conc_t)
+
 for i in 1:6
-    temp = dde_params[i]
+    temp = dde_paramsL[i]
+    @test all(x -> x>0, temp)
+    temp = dde_paramsD[i]
+    @test all(x -> x>0, temp)
+    temp = dde_paramsG[i]
+    @test all(x -> x>0, temp)
+    temp = dde_paramsT[i]
     @test all(x -> x>0, temp)
 end
 
 # profiling for Hill model
-@profile optimize_hill(guess, concentrations, g1, g2, g1_0, g2_0, num_steps)
+@profile optimize_hill(guess, conc_l, g1l, g2l, g1_0l, g2_0l, num_steps)
 Profile.print(noisefloor=10.0)
