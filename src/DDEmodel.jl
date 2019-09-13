15f8feb2436b27336bf9a973dce32054336cf86f
farnazmdi <farnazmd95@gmail.com>
2019-09-13 16:09:25 -0700

    changed dde model but doesn't work yet

--- src/DDEmodel.jl
+++ src/DDEmodel.jl
@@ -31,7 +31,7 @@ function prob_generator(prob, p)
     remake(prob; p = exp_p, constant_lags = [exp_p[3], exp_p[4]])
 end
 
-
+# this function is used for Hill fitting
 function ddesolve(g1, g2, g1_0, g2_0, params, j)
     times = range(0.0; stop = 95.5, length = 192)
     data = vcat(g1[:, j]', g2[:, j]')
@@ -54,30 +54,49 @@ function ddesolve(g1, g2, g1_0, g2_0, params, j)
     return obj(params)
 end
 
-function optimization(g1, g2, g1_0, g2_0, initial_guess, j, lower, upper, num_steps)
+# this function is used for DDE fitting
+function DDEsolve(initial_guess, g1, g2, g1_0, g2_0, j)
+
     times = range(0.0; stop = 95.5, length = 192)
-    data = vcat(g1[:, j]', g2[:, j]')
-    
     # history function
     fit1, fit2 = find_history(g1, g2)
     h(p, t) = [exp_model(t, fit1); exp_model(t, fit2)]
-    bound = collect(zip(lower, upper))
+    pars = append!(initial_guess[4*j - 3 : 4*j], [initial_guess[33], initial_guess[34]])
     # problem
-    prob = DDEProblem(DDEmodel, [g1_0[j], g2_0[j]], h, extrema(times), initial_guess;
-                      constant_lags = [30.0, 8.0])
-    # algorithm to solve
+    prob = DDEProblem(DDEmodel, [g1_0[j], g2_0[j]], h, extrema(times), pars;
+                          constant_lags = [initial_guess[33], initial_guess[34]])
     alg = MethodOfSteps(AutoTsit5(Rosenbrock23()); constrained=true)
 
-    # objective function
-    obj = build_loss_objective(prob, alg, L2Loss(times, data);
-                               prob_generator=prob_generator,
-                               verbose_opt = false)
+    println("hello")
+    solve(prob)
+    println("hello again")
+end
+
+function cost_function(initial_guess, g1, g2, g1_0, g2_0)
+    times = range(0.0; stop = 95.5, length = 192)
+
+    total_cost = zeros(8)
+    for j in 1:8
+        sol = DDEsolve(initial_guess, g1, g2, g1_0, g2_0, j)
+        res1 = sum((sol(t, idxs=1).u - g1[:, j]).^2)
+        res2 = sum((sol(t, idxs=2).u - g2[:, j]).^2)
+        total_cost[j] = res1 + res2
+    end
+    return sum(total_cost)
+end
+
+function optimization(g1, g2, g1_0, g2_0, initial_guess, lower, upper, num_steps)
+
+    # the cost function must just be a function of parameters
+    cost_function(initial_guess) = cost_function(initial_guess, g1, g2, g1_0, g2_0)
+    bound = collect(zip(lower, upper))
+
     # optimizing
-    results_dde = bboptimize(obj; SearchRange=bound,
-                                    NumDimensions=4,
-                                    TraceInterval=100,
-                                    MasSteps=num_steps,
-                                    Method=:adaptive_de_rand_1_bin_radiuslimited)
+    results_dde = bboptimize(cost_function; SearchRange=bound,
+                                        NumDimensions=34,
+                                        TraceInterval=100,
+                                        MasSteps=num_steps,
+                                        Method=:adaptive_de_rand_1_bin_radiuslimited)
 
     new_guess = best_candidate(results_dde)
     return best_fitness(results_dde), exp.(new_guess)
