8bcd5cb45f3903695e10c7cf6f60af8feab5ba96
farnazmdi <farnazmd95@gmail.com>
2019-09-15 20:44:20 -0700

    hill changed based on constant delay

--- src/Hill.jl
+++ src/Hill.jl
@@ -38,16 +38,14 @@ function optimize_hill(guess, concentrations, g1, g2, g1_0, g2_0, num_steps)
     # changing the objective function to be compatible with bboptimize
     residue(hillParams) = residHill(hillParams, concentrations, g1, g2, g1_0, g2_0)
     # lower bound
-    low = [50.0, 0.01, 0.005, 0.04, 0.005, 0.01, 10.0, 4.0, 0.0001, 0.0001]
+    low = [50.0, 0.01, 0.005, 0.04, 0.005, 0.01, 20.0, 5.0, 0.0001, 0.0001]
     # upper bound
-    high = [250, 2.0, 0.02, 0.1, 0.2, 0.03, 40.0, 10.0, 0.05, 0.1]
+    high = [250, 2.0, 0.02, 0.1, 0.2, 0.03, 40.0, 20.0, 0.05, 0.1]
 
     println("global optimization begins ...")
     res = bboptimize(residue; SearchRange=collect(zip(low, high)), MaxSteps=num_steps, TraceInterval=50, Method =:adaptive_de_rand_1_bin_radiuslimited)
     new_guess = best_candidate(res)
     return best_fitness(res), new_guess
-#     optim_res = optimize(residue, low, high, new_guess, Fminbox(), Optim.Options(show_trace=true))
-#     return Optim.minimum(optim_res), Optim.minimizer(optim_res)
 end
 
 function getDDEparams(p, concentrations)
