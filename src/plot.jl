82d20c21071df2edb3ffb0009dfbb8dce9e72ed2
farnazmdi <farnazmd95@gmail.com>
2019-09-17 10:27:43 -0700

    plot.jl updated

--- src/plot.jl
+++ src/plot.jl
@@ -1,13 +1,13 @@
 using Plots, CSV, DataFrames
 include("importData.jl")
 include("DDEmodel.jl")
-gr()
+pyplot()
 """ 
         This file contains a function to plot the parameters against the  drug concentrations, in a 2x2 subplot.
 """
 
 #-------------------- Plots the long-term predicted behavior given parameters -----------------------#
-function plotIt(params::Array, i::Int, title::String, bool::Any)
+function plotIt(params::Array, i, title::String, bool::Any)
     """ Given estimated parameters for each trial, 
     solve the DDE model plot the predicted curve 
     for # of cells in G1, G2, or total, 
@@ -26,10 +26,11 @@ function plotIt(params::Array, i::Int, title::String, bool::Any)
     
     solution = solve(prob_new, MethodOfSteps(AutoTsit5(Rosenbrock23()); constrained=true))
 
-    plot(n_times, solution(n_times, idxs=1).u, label = "G1 est", dpi = 150, title = title, xlabel = "time [hours]", ylabel = "# of cells", lw=2.0, alpha = 0.6)
-    scatter!(times, g1[:, i], label = "G1", dpi = 150, markersize = 1.0, marker=([:dot :d], 1, 0.8, Plots.stroke(0.1, :gray)))
-    plot!(n_times, solution(n_times, idxs=2).u, label = "G2 est", legend=bool, legendfontsize=5, fg_legend = :transparent, lw=2.0, alpha = 0.6)
-    scatter!(times, g2[:, i], label = "G2", dpi = 150, markersize = 1.0, marker=([:dot :d], 1, 0.8, Plots.stroke(0.1, :gray)))
-    plot!(n_times, (solution(n_times, idxs=2).u + solution(n_times, idxs=1).u), label = "total est", dpi = 150, lw=2.0, alpha = 0.6)
-    scatter!(times, pop[i], label = "total", dpi = 150, markersize = 1.0, marker=([:dot :d], 1, 0.8, Plots.stroke(0.1, :gray)))
+    plot(n_times, solution(n_times, idxs=1).u, label = "G1 est", dpi = 150, xlabel = "time [hours]", ylabel = "# of cells", lw=2.0, alpha = 0.6, color =:green)
+    plot!(times, g1[:, i], label = "G1", dpi = 150, markersize = 1.0, color=:darkgreen)
+    plot!(n_times, solution(n_times, idxs=2).u, label = "G2 est", legend=bool, legendfontsize=7, fg_legend = :transparent, lw=2.0, alpha = 0.6, color=:sienna)
+    plot!(times, g2[:, i], label = "G2", dpi = 150, markersize = 1.0, color=:darkorange)
+    plot!(n_times, (solution(n_times, idxs=2).u + solution(n_times, idxs=1).u), label = "total est", dpi = 150, lw=2.0, alpha = 0.6, color=:hotpink)
+    plot!(times, pop[i], label = "total", dpi = 150, markersize = 1.0, color=:indigo)
+    plot!( annotation=[ (75,90, text(title, 12)) ])
 end
