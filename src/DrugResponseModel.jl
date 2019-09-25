5c9d513280f127cf93f16ac7dd3c7dc64cea47f9
Aaron Meyer <aaron@asmlab.org>
2019-09-25 08:17:16 -0700

    precompile

--- src/DrugResponseModel.jl
+++ src/DrugResponseModel.jl
@@ -11,6 +11,6 @@ include("Hill.jl")
 
 export setup_data, remove_peaks, find_history, ODEoptimizer, ode_plotIt, ddesolve, optimization, plotIt, residHill, optimize_hill, getDDEparams, correlationPlot
 
-
+precompile(optimization, (Matrix{Float64}, Matrix{Float64}, Vector{Float64}, Vector{Float64}, Vector{Float64}, Int, Vector{Float64}, Vector{Float64}, Int))
 
 end # module
