72135f1f5b957c2f1a0edf7773d17d238b60d3cb
Farnazm <34812750+Farnazmdi@users.noreply.github.com>
2019-09-25 16:04:26 -0700

    Update testODE.jl

--- test/testODE.jl
+++ test/testODE.jl
@@ -1,3 +1,6 @@
+using Test
+using Profile
+using DrugResponseModel
 
 println("####################  ODE model tests begin ... ")
 
@@ -20,5 +23,4 @@ for i in 1:8
 end
 params_ode = ODEoptimizer(low, upp, p, 5, g1, g2, g1_0, g2_0)
 @profile ODEoptimizer(low, upp, p, 5, g1, g2, g1_0, g2_0)
-@profile ode_plotIt(params_ode, g1, g2, g1_0, g2_0, pop, 8, "", false)
-
+@profile ode_plotIt(params_ode, g1, g2, g1_0, g2_0, pop, 8, "")
