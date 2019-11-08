a50e8e0bfec059aa8c6f290f6baeb42099c35d8a
farnazmdi <farnazmd95@gmail.com>
2019-11-08 11:45:30 -0800

    added new functions to export

--- src/DrugResponseModel.jl
+++ src/DrugResponseModel.jl
@@ -9,7 +9,7 @@ include("plot.jl")
 include("Hill.jl")
 
 
-export setup_data, remove_peaks, find_history, ODEoptimizer, ode_plotIt, ddesolve, optimization, plotIt, residHill, optimize_hill, getDDEparams, correlationPlot, plot_all, plot_parameters, ODEplot_all
+export setup_data, remove_peaks, find_history, ODEoptimizer, ode_plotIt, ddesolve, optimization, plotIt, residHill, optimize_hill, getDDEparams, ParamForBliss, BlissCombination, correlationPlot, plot_all, plot_parameters, ODEplot_all, plot3D
 
 precompile(optimization, (Matrix{Float64}, Matrix{Float64}, Vector{Float64}, Vector{Float64}, Vector{Float64}, Int, Vector{Float64}, Vector{Float64}, Int))
 
