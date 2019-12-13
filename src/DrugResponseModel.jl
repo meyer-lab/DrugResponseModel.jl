74357b70ff0e6c3094dbc86b07eda9587b787c1a
Farnazm <farnazmd95@gmail.com>
2019-12-12 21:11:36 -0800

    ODE 4eq (#66)
    * added ODE4
    
    * deleting images
    
    * Update ODE4.jl
    
    * first commit
    
    * Hill ode (#67)
    
    * added ODE4
    
    * deleting images
    
    * Update ODE4.jl
    
    * Update ODE4.jl
    
    * changes

--- src/DrugResponseModel.jl
+++ src/DrugResponseModel.jl
@@ -16,11 +16,12 @@ using DiffEqBase
 
 include("importData.jl")
 include("ODEmodel.jl")
+include("ODE4.jl")
 include("plot.jl")
 include("DDEmodel.jl")
 include("Hill.jl")
 
-export setup_data, remove_peaks, find_history, ODEoptimizer, ode_plotIt, ddesolve, optimization, plotIt, residHill, optimize_hill, getDDEparams, ParamForBliss, BlissCombination, correlationPlot, plot_all, plot_parameters, ODEplot_all
+export setup_data, remove_peaks, find_history, ODEoptimizer, ode_plotIt, ddesolve, optimization, plotIt, residHill, optimize_hill, getDDEparams, ParamForBliss, BlissCombination, correlationPlot, plot_all, plot_parameters, ODEplot_all, ODEoptimizer4, ode_plotIt4, update_coef4, ODEmodel4, cost, predict, ODEplot_all4
 
 precompile(optimization, (Matrix{Float64}, Matrix{Float64}, Vector{Float64}, Vector{Float64}, Vector{Float64}, Int, Vector{Float64}, Vector{Float64}, Int))
 
