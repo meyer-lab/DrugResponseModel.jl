63c50e545f7043a2cb348382d63237917e3dd1fe
Aaron Meyer <aaron@asmlab.org>
2019-12-16 10:36:59 -0800

    Remove more.

--- src/DrugResponseModel.jl
+++ src/DrugResponseModel.jl
@@ -6,7 +6,6 @@ using CSV
 using DataFrames
 using Plots
 using Measures
-using LsqFit
 using DelayDiffEq
 using DiffEqParamEstim
 using BlackBoxOptim
@@ -18,10 +17,8 @@ include("importData.jl")
 include("ODEmodel.jl")
 include("ODE4.jl")
 include("plot.jl")
-include("DDEmodel.jl")
-include("Hill.jl")
 
-export setup_data, remove_peaks, find_history, ODEoptimizer, ode_plotIt, ddesolve, optimization, plotIt, residHill, optimize_hill, getDDEparams, ParamForBliss, BlissCombination, correlationPlot, plot_all, plot_parameters, ODEplot_all, ODEoptimizer4, ode_plotIt4, update_coef4, ODEmodel4, cost, predict, ODEplot_all4
+export setup_data, remove_peaks, ODEoptimizer, ode_plotIt, optimization, plotIt, ParamForBliss, BlissCombination, correlationPlot, plot_all, plot_parameters, ODEplot_all, ODEoptimizer4, ode_plotIt4, update_coef4, ODEmodel4, cost, predict, ODEplot_all4
 
 precompile(optimization, (Matrix{Float64}, Matrix{Float64}, Vector{Float64}, Vector{Float64}, Vector{Float64}, Int, Vector{Float64}, Vector{Float64}, Int))
 
