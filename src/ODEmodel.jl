78c645e96a5e362d498542e916136e73ec4fad85
Aaron Meyer <aaron@asmlab.org>
2019-12-18 07:31:04 -0800

    Try this.

--- src/ODEmodel.jl
+++ src/ODEmodel.jl
@@ -19,22 +19,22 @@ function predict(p, g1_0::Real, g2_0::Real, t, nG1::Int, nG2::Int)
     @assert t[1] == 0.0
 
     v = vec([ones(nG1)*g1_0/nG1; ones(nG2)*g2_0/nG2])
-    vv = similar(v)
     A = ODEjac(p, nG1, nG2)
 
     G1 = zeros(length(t))
     G2 = zeros(length(t))
 
-    lmul!(t[2], A)
-    LinearAlgebra.exp!(A)
+    @inbounds (
+        lmul!(t[2], A)
+        LinearAlgebra.exp!(A)
 
-    for ii in 1:length(G1)
-        G1[ii] = sum(v[1:nG1])
-        G2[ii] = sum(v[nG1+1:nG1+nG2])
+        for ii in 1:length(G1)
+            G1[ii] = sum(v[1:nG1])
+            G2[ii] = sum(v[nG1+1:nG1+nG2])
 
-        LinearAlgebra.mul!(vv, A, v)
-        copyto!(v, vv)
-    end
+            v = A*v
+        end
+    )
 
     return G1, G2
 end
