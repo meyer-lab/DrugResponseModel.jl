d655979485181ca5946aa3758d48968d95d7dc59
farnazmdi <farnazmd95@gmail.com>
2019-12-18 17:58:27 -0800

    replaced Hilloptimization

--- src/Hill.jl
+++ src/Hill.jl
@@ -26,22 +26,34 @@ function residHill(hillParams::Array{Float64,1}, concentrations::Array{Float64,1
     return residues
 end
 
-# optimization function for the Hill model
-function optimize_hill(guess::Array{Float64,1}, concentrations::Array{Float64,1}, g1::Array{Float64,2}, g2::Array{Float64,2}, g1_0::Array{Float64,1}, g2_0::Array{Float64,1}, lowEC50::Float64, highEC50::Float64, nG1::Int, nG2::Int)
-    """ A function to do the optimization given the lower and upper bound of estimation space. """
-    # changing the objective function to be compatible with bboptimize
-    residue(hillParams) = residHill(hillParams, concentrations, g1, g2, g1_0, g2_0, nG1, nG2)
-    # lower bound
-    low = [lowEC50, 1e-5, 1e-5, 1e-5, 1e-5, 1e-5, lowEC50, 1e-5, 1e-5, 1e-5, 0.0]
-    # upper bound
-    high = [highEC50, 3.0, 3.0, 3.0, 3.0, 3.0, highEC50, 3.0, 3.0, 3.0, 1.0]
-
-    res = bboptimize(residue; SearchRange=collect(zip(low, high)), MaxSteps=60000, TraceMode=:silent)
-    return best_fitness(res), best_candidate(res)
+""" This helps finding the optimum number of species. """
+function fullHillCost(hillParams::Array{Float64,1}, conc_l::Array{Float64,1}, g1::Array{Float64,2}, g2::Array{Float64,2}, g1_0::Array{Float64,1}, g2_0::Array{Float64,1})
+    nG1 = floor(hillParams[end-1])
+    nG2 = floor(hillParams[end])
+    
+    return DrugResponseModel.residHill(hillParams[1:end-2], conc_l, g1, g2, g1_0, g2_0, Int(nG1), Int(nG2))
 end
 
+""" Hill optimization function. """
+function optimize_hill(lowEC50::Float64, highEC50::Float64, conc_l::Array{Float64,1}, g1::Array{Float64,2}, g2::Array{Float64,2}, g1_0::Array{Float64,1}, g2_0::Array{Float64,1})
+    lowEC50 = 50.0
+    highEC50 = 350.0
+    hillCost(hillParams) = fullHillCost(hillParams, conc_l, g1, g2, g1_0, g2_0) 
+    low =  [lowEC50, 1e-5, 1e-5, 1e-5, 1e-5, 1e-5, lowEC50, 1e-5, 1e-5, 1e-5, 0.0, 1, 1]
+    high = [highEC50, 3.0, 3.0, 3.0, 3.0, 3.0, highEC50, 3.0, 3.0, 3.0, 1.0, 60, 60]
+
+    results_ode = bboptimize(hillCost; SearchRange=collect(zip(low, high)),
+                                           NumDimensions=length(low),
+                                           TraceMode=:verbose,
+                                           TraceInterval=50,
+                                           MaxSteps=4E4);
+
+    return best_candidate(results_ode)
+end
+
+""" A function to convert the estimated hill parameters back to ODE parameters. """
 function getODEparams(p::Array{Float64,1}, concentrations::Array{Float64,1})
-    """ A function to convert the estimated hill parameters back to ODE parameters. """
+
     effects = zeros(5, 8)
     for i in 1:8
         effects[1, i] = hill([p[1], p[2], p[3], p[4]], concentrations[i])
@@ -50,5 +62,7 @@ function getODEparams(p::Array{Float64,1}, concentrations::Array{Float64,1})
         effects[4, i] = hill([p[7], 0.0, p[10], p[9]], concentrations[i])
         effects[5, i] = p[11]
     end
-    return effects
+    nG1 = p[end-1]
+    nG2 = p[end]
+    return Int(floor(nG1)), Int(floor(nG2)), effects
 end
--- test/testHill.jl
+++ test/testHill.jl
@@ -1,19 +1,17 @@
 @testset "Hill tests" begin
 	conc, pop, g2, g1, g1_0, g2_0 = setup_data("lapatinib")
 
-	t = range(0.0; stop = 95.5, length = 192)
-    guess = [100.0, 1.0, 1.0, 1.0, 100, 1.0, 1.0, 1.0, 100, 1.0, 1.0, 100.0, 1.0, 1.0, 0.5]
     # run the optimization
-    fitness, params = optimize_hill(guess, conc, g1, g2, g1_0, g2_0, 50.0, 300.0, 1, 1)
+    fitness, params = optimize_hill(50.0, 350.0, conc_l, g1, g2, g1_0, g2_0)
     println(fitness)
 
-	@time optimize_hill(guess, conc, g1, g2, g1_0, g2_0, 50.0, 300.0, 1, 1)
+	@time optimize_hill(50.0, 350.0, conc, g1, g2, g1_0, g2_0)
 
-	@profile optimize_hill(guess, conc, g1, g2, g1_0, g2_0, 50.0, 300.0, 1, 1)
+	@profile optimize_hill(50.0, 350.0, conc, g1, g2, g1_0, g2_0)
 
 	Profile.print(noisefloor=5.0)
 
     # Check that these at least run
-    effects = getODEparams(params, conc)
+    nG1, nG2, effects = getODEparams(params, conc)
 	plot_parameters(conc, effects)
 end
