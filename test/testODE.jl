69f72293915597afd233f9cd1721a4d4f9bc9acc
farnazmdi <farnazmd95@gmail.com>
2019-12-19 11:18:23 -0800

    tiny fix in test

--- test/testODE.jl
+++ test/testODE.jl
@@ -2,7 +2,7 @@
 	_, pop, g2, g1, g1_0, g2_0 = setup_data("lapatinib")
 
 	t = range(0.0; stop = 95.5, length = 192)
-	DrugResponseModel.predict([1.0, 1.1, 0.2, 0.3, 0.5], 1.0, 1.0, t)
+	DrugResponseModel.predict([1.0, 1.1, 0.2, 0.3, 0.5, 20, 20], 1.0, 1.0, t)
 
 	# ODE optimization and estimation of the parameters
 	fitness, params_ode = ODEoptimizer(4, g1, g2, g1_0, g2_0)
