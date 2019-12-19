2da62806f3c9f4a6699c60664233ed02a4ac8c9f
farnazmdi <farnazmd95@gmail.com>
2019-12-19 15:04:50 -0800

    edits

--- src/ODEmodel.jl
+++ src/ODEmodel.jl
@@ -17,14 +17,12 @@ end
 
 
 """ Predicts the model given a set of parametrs. """
-function predict(p, g1_0::Real, g2_0::Real, t)
+function predict(p, g1_0::Real, g2_0::Real, t, nG1::Int, nG2::Int)
     # Some assumptions
     @assert t[1] == 0.0
 
-    nG1 = Int(floor(p[6]))
-    nG2 = Int(floor(p[7]))
-    v = [ones(nG1)*p[5]*(g1_0+g2_0)/nG1; ones(nG2)*(1.0-p[5])*(g1_0+g2_0)/nG2]
-    A = ODEjac(p[1:4], t[2], nG1, nG2)
+    v = [ones(nG1)*g1_0/nG1; ones(nG2)*g2_0/nG2]
+    A = ODEjac(p, t[2], nG1, nG2)
 
     G1 = Vector{eltype(p)}(undef, length(t))
     G2 = Vector{eltype(p)}(undef, length(t))
@@ -41,12 +39,10 @@ end
 
 
 """ Calculates the cost function for a given set of parameters. """
-function cost(p, g1_0, g2_0, g1, g2)
-    nG1 = Int(floor(p[6]))
-    nG2 = Int(floor(p[7]))
-    v = [ones(nG1)*p[5]*(g1_0+g2_0)/nG1; ones(nG2)*(1.0-p[5])*(g1_0+g2_0)/nG2]
+function cost(p, g1_0, g2_0, g1, g2, nG1::Int, nG2::Int)
+    v = [ones(nG1)*g1_0/nG1; ones(nG2)*g2_0/nG2]
     temp = similar(v)
-    A = ODEjac(p[1:4], 0.5, nG1, nG2)
+    A = ODEjac(p, 0.5, nG1, nG2)
 
     cost = 0.0
     for ii in 1:length(g1)
@@ -63,7 +59,7 @@ end
 
 """ Fit the ODE model to data. """
 function ODEoptimizer(i::Int, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array)
-    residuals(p) = cost(p, g1_0[i], g2_0[i], g1[:, i], g2[:, i])
+    residuals(p) = cost(p, g1_0[i], g2_0[i], g1[:, i], g2[:, i], Int(floor(p[6])), Int(floor(p[7])))
     # lower and upper bounds for the parameters
     lower = [0.0, 0.0, 0.0, 0.0, 0.0, 1, 1]
     upper = [3.0, 3.0, 3.0, 3.0, 1.0, 70, 70]
@@ -79,10 +75,10 @@ end
 """ Given estimated parameters for each trial, solve the DDE model plot the predicted curve 
     for number of cells in G1, G2, or total, along with their corresponding real data,
     for a longer time which is 2 times of the original time (~195 hours) """
-function ode_plotIt(params::Vector{Float64}, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array, pop, i::Int, title::String, legend::Any)
+function ode_plotIt(params::Vector, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array, pop, i::Int, title::String, legend::Any)
     t = LinRange(0.0, 95.5, 192)
     t_new = LinRange(0.0, 150, 200)
-    G1, G2 = predict(params, g1_0[i], g2_0[i], t_new)
+    G1, G2 = predict(params, g1_0[i], g2_0[i], t_new, Int(floor(params[6])), Int(floor(params[7])))
 
     plot(t_new, G1, label = "G1 est", dpi = 150, xlabel = "time [hours]", ylabel = "# of cells", lw=2.0, alpha = 0.6, color=:green)
     plot!(t, g1[:, i], label = "G1", dpi = 150, markersize = 1.0, color=:darkgreen)
