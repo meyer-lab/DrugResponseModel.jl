4fe82059a0ca5ee95e7f675c4cebe65edabaed1b
farnazmdi <farnazmd95@gmail.com>
2019-12-19 11:07:38 -0800

    nG1 nG2 are estimated in ODE now

--- src/ODEmodel.jl
+++ src/ODEmodel.jl
@@ -4,7 +4,7 @@
 
 """ Make the transition matrix. """
 function ODEjac(p, dt::Real, nG1::Int, nG2::Int)
-    # p = [alpha, beta, gamma1, gamma2]
+    # p = [alpha, beta, gamma1, gamma2, nG1, nG2]
     A = diagm( 0 => vec([-ones(nG1)*(p[3]+p[1]); -ones(nG2)*(p[4]+p[2])]),
               -1 => vec([ones(nG1)*p[1];  ones(nG2-1)*p[2]]))
     A[1, end] = 2*p[2]
@@ -17,10 +17,12 @@ end
 
 
 """ Predicts the model given a set of parametrs. """
-function predict(p, g1_0::Real, g2_0::Real, t, nG1::Int, nG2::Int)
+function predict(p, g1_0::Real, g2_0::Real, t)
     # Some assumptions
     @assert t[1] == 0.0
 
+    nG1 = Int(floor(p[6]))
+    nG2 = Int(floor(p[7]))
     v = [ones(nG1)*p[5]*(g1_0+g2_0)/nG1; ones(nG2)*(1.0-p[5])*(g1_0+g2_0)/nG2]
     A = ODEjac(p[1:4], t[2], nG1, nG2)
 
@@ -39,7 +41,9 @@ end
 
 
 """ Calculates the cost function for a given set of parameters. """
-function cost(p, g1_0, g2_0, g1, g2, nG1::Int, nG2::Int)
+function cost(p, g1_0, g2_0, g1, g2)
+    nG1 = Int(floor(p[6]))
+    nG2 = Int(floor(p[7]))
     v = [ones(nG1)*p[5]*(g1_0+g2_0)/nG1; ones(nG2)*(1.0-p[5])*(g1_0+g2_0)/nG2]
     temp = similar(v)
     A = ODEjac(p[1:4], 0.5, nG1, nG2)
@@ -58,14 +62,15 @@ end
 
 
 """ Fit the ODE model to data. """
-function ODEoptimizer(i::Int, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array, nG1::Int, nG2::Int)
-    residuals(p) = cost(p, g1_0[i], g2_0[i], g1[:, i], g2[:, i], nG1, nG2)
+function ODEoptimizer(i::Int, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array)
+    residuals(p) = cost(p, g1_0[i], g2_0[i], g1[:, i], g2[:, i])
     # lower and upper bounds for the parameters
-    upper = [nG1*0.1, nG1*0.1, nG1*0.1, nG1*0.1, 1.0]
-    bound = collect(zip(zeros(5), upper))
+    lower = [0.0, 0.0, 0.0, 0.0, 0.0, 1, 1]
+    upper = [3.0, 3.0, 3.0, 3.0, 1.0, 70, 70]
+    bound = collect(zip(lower, upper))
 
     # global optimization with black box optimization
-    results_ode = bboptimize(residuals; SearchRange=bound, NumDimensions=5, TraceMode=:silent, MaxSteps=50000)
+    results_ode = bboptimize(residuals; SearchRange=bound, NumDimensions=7, TraceMode=:silent, MaxSteps=50000)
 
     return best_fitness(results_ode), best_candidate(results_ode)
 end
@@ -74,10 +79,10 @@ end
 """ Given estimated parameters for each trial, solve the DDE model plot the predicted curve 
     for number of cells in G1, G2, or total, along with their corresponding real data,
     for a longer time which is 2 times of the original time (~195 hours) """
-function ode_plotIt(params::Vector{Float64}, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array, pop, i::Int, title::String, legend::Any, nG1::Int, nG2::Int)
+function ode_plotIt(params::Vector{Float64}, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array, pop, i::Int, title::String, legend::Any)
     t = LinRange(0.0, 95.5, 192)
     t_new = LinRange(0.0, 150, 200)
-    G1, G2 = predict(params, g1_0[i], g2_0[i], t_new, nG1, nG2)
+    G1, G2 = predict(params, g1_0[i], g2_0[i], t_new)
 
     plot(t_new, G1, label = "G1 est", dpi = 150, xlabel = "time [hours]", ylabel = "# of cells", lw=2.0, alpha = 0.6, color=:green)
     plot!(t, g1[:, i], label = "G1", dpi = 150, markersize = 1.0, color=:darkgreen)
@@ -90,10 +95,10 @@ end
 
 
 """ Plot the data and curves for all concentrations. """
-function ODEplot_all(params_ode, g1_l::Matrix, g2_l::Matrix, g1_0_l::Array, g2_0_l::Array, pop_l, nG1::Int, nG2::Int)
+function ODEplot_all(params_ode, g1_l::Matrix, g2_l::Matrix, g1_0_l::Array, g2_0_l::Array, pop_l)
     # plotting the fitted curves
-    rl = [ode_plotIt(params_ode[:, i], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, i, "", false, nG1, nG2) for i in 1:7]
-    r8 = ode_plotIt(params_ode[:, 8], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, 8, "", :topleft, nG1, nG2)
+    rl = [ode_plotIt(params_ode[:, i], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, i, "", false) for i in 1:7]
+    r8 = ode_plotIt(params_ode[:, 8], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, 8, "", :topleft)
     plot(rl..., r8, layout = (2,4))
     plot!(size=(900, 400), layout = (4,2), dpi=200)
     ylims!((0.0, 120.0))
