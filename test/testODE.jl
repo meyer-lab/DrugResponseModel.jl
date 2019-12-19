1af1d59074c140ab100317797e3f46724bbc6029
farnazmdi <farnazmd95@gmail.com>
2019-12-19 11:09:10 -0800

    tests updated

--- test/testODE.jl
+++ test/testODE.jl
@@ -2,18 +2,18 @@
 	_, pop, g2, g1, g1_0, g2_0 = setup_data("lapatinib")
 
 	t = range(0.0; stop = 95.5, length = 192)
-	DrugResponseModel.predict([1.0, 1.1, 0.2, 0.3, 0.5], 1.0, 1.0, t, 3, 3)
+	DrugResponseModel.predict([1.0, 1.1, 0.2, 0.3, 0.5], 1.0, 1.0, t)
 
 	# ODE optimization and estimation of the parameters
-	fitness, params_ode = ODEoptimizer(4, g1, g2, g1_0, g2_0, 1, 1)
+	fitness, params_ode = ODEoptimizer(4, g1, g2, g1_0, g2_0)
 	println(fitness)
 
-	@time ODEoptimizer(4, g1, g2, g1_0, g2_0, 3, 3)
+	@time ODEoptimizer(4, g1, g2, g1_0, g2_0)
 
-	@profile ODEoptimizer(4, g1, g2, g1_0, g2_0, 3, 3)
+	@profile ODEoptimizer(4, g1, g2, g1_0, g2_0)
 
 	Profile.print(noisefloor=5.0)
 
 	# Check that these at least run
-	ode_plotIt(params_ode, g1, g2, g1_0, g2_0, pop, 4, "", false, 1, 1)
+	ode_plotIt(params_ode, g1, g2, g1_0, g2_0, pop, 4, "", false)
 end
