5207160c81a366aa223ae8236cc2f30cdc71abcc
farnazmdi <farnazmd95@gmail.com>
2019-12-18 16:42:11 -0800

    tests updated

--- test/testHill.jl
+++ test/testHill.jl
@@ -2,7 +2,7 @@
 	conc, pop, g2, g1, g1_0, g2_0 = setup_data("lapatinib")
 
 	t = range(0.0; stop = 95.5, length = 192)
-    guess = [100.0, 1.0, 1.0, 1.0, 100, 1.0, 1.0, 1.0, 100, 1.0, 1.0, 100.0, 1.0, 1.0]
+    guess = [100.0, 1.0, 1.0, 1.0, 100, 1.0, 1.0, 1.0, 100, 1.0, 1.0, 100.0, 1.0, 1.0, 0.5]
     # run the optimization
     fitness, params = optimize_hill(guess, conc, g1, g2, g1_0, g2_0, 50.0, 300.0, 1, 1)
     println(fitness)
