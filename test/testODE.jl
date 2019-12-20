cca781b6ace270e4c51684614218031d3775ece7
Aaron Meyer <aaron@asmlab.org>
2019-12-20 07:27:56 -0800

    Commit.

--- test/testODE.jl
+++ test/testODE.jl
@@ -8,12 +8,6 @@
 	fitness, params_ode = ODEoptimizer(4, g1, g2, g1_0, g2_0)
 	println(fitness)
 
-	@time ODEoptimizer(4, g1, g2, g1_0, g2_0)
-
-	@profile ODEoptimizer(4, g1, g2, g1_0, g2_0)
-
-	Profile.print(noisefloor=5.0)
-
 	# Check that these at least run
 	ode_plotIt(params_ode, g1, g2, g1_0, g2_0, pop, 4, "", false)
 end
