8d09ebc6afd3cce0dd5e7b648d1c8f09c6d79e13
Farnazm <farnazmd95@gmail.com>
2019-12-19 21:03:45 -0800

    plot hill. (#86)
    * plot hill.
    
    * hill notebook updated
    
    * hill after flexible intis
    
    * tests updated
    
    * replaced Hilloptimization
    
    * quick remove
    
    * quick fix
    
    * one tiny error
    
    * getODEparams into the residHill
    
    * nG1 nG2 are estimated in ODE now
    
    * tests updated
    
    * tiny fix in test
    
    * flexible nG1 nG2 added to ODE
    
    * edits
    
    * more edits
    
    * one tiny fix
    
    * another error
    
    * fix
    
    * another error
    
    * Update Hill.jl
    
    * Update Hill.jl
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- src/Hill.jl
+++ src/Hill.jl
@@ -4,49 +4,51 @@ This file fits Hill function to the parameters.
 
 hill(p, concentration) = p[2] + ((p[3]-p[2])/(1 + ((p[1])/(concentration))^p[4]))
 
-function residHill(hillParams::Array{Float64,1}, concentrations::Array{Float64,1}, g1::Array{Float64,2}, g2::Array{Float64,2}, g1_0::Array{Float64,1}, g2_0::Array{Float64,1}, nG1::Int, nG2::Int)
-    """ This functions takes in hill parameters for all the concentrations and calculates
-    DDE parameters, passes them to residual function and based off of these, optimizes the model
-    and estimates hill parameters. """
+""" This functions takes in hill parameters for all the concentrations and calculates
+DDE parameters, passes them to residual function and based off of these, optimizes the model
+and estimates hill parameters. """
+function residHill(hillParams::Array{Float64,1}, concentrations::Array{Float64,1}, g1::Array{Float64,2}, g2::Array{Float64,2}, g1_0::Array{Float64,1}, g2_0::Array{Float64,1})
     residues = 0.0
 
+    params = getODEparams(hillParams, concentrations)
     for ii in 1:length(concentrations)
-        # [EC50, left, right, steepness]
-        alpha =  hill([hillParams[1], hillParams[2], hillParams[3], hillParams[4]], concentrations[ii])
-        beta =   hill([hillParams[1], hillParams[5], hillParams[6], hillParams[4]], concentrations[ii])
-        gamma1 = hill([hillParams[7], 0.0, hillParams[8], hillParams[9]], concentrations[ii])
-        gamma2 = hill([hillParams[7], 0.0, hillParams[10], hillParams[9]], concentrations[ii])
-
-        # collecting all the DDE model parameters
-        pp = [alpha, beta, gamma1, gamma2]
         # calculating the residulas for this set of parameters
-        residues += cost(pp, g1_0[ii], g2_0[ii], g1[:,ii], g2[:,ii], nG1, nG2)
+        residues += cost(params[:, ii], g1_0[ii], g2_0[ii], g1[:,ii], g2[:,ii], Int(floor(params[6, ii])), Int(floor(params[7, ii])))
     end 
     return residues
 end
 
-# optimization function for the Hill model
-function optimize_hill(guess::Array{Float64,1}, concentrations::Array{Float64,1}, g1::Array{Float64,2}, g2::Array{Float64,2}, g1_0::Array{Float64,1}, g2_0::Array{Float64,1}, lowEC50::Float64, highEC50::Float64, nG1::Int, nG2::Int)
-    """ A function to do the optimization given the lower and upper bound of estimation space. """
-    # changing the objective function to be compatible with bboptimize
-    residue(hillParams) = residHill(hillParams, concentrations, g1, g2, g1_0, g2_0, nG1, nG2)
-    # lower bound
-    low = [lowEC50, 1e-5, 1e-5, 1e-5, lowEC50, 1e-5, 1e-5, 1e-5, lowEC50, 1e-5, 1e-5, 1e-5]
-    # upper bound
-    high = [highEC50, 3.0, 3.0, 3.0, highEC50, 3.0, 3.0, 3.0, highEC50, 3.0, 3.0, 3.0]
+""" Hill optimization function. """
+function optimize_hill(lowEC50::Float64, highEC50::Float64, conc_l::Array{Float64,1}, g1::Array{Float64,2}, g2::Array{Float64,2}, g1_0::Array{Float64,1}, g2_0::Array{Float64,1})
+    lowEC50 = 50.0
+    highEC50 = 350.0
+    hillCost(hillParams) = residHill(hillParams, conc_l, g1, g2, g1_0, g2_0)
+#     hillCost(hillParams) = fullHillCost(hillParams, conc_l, g1, g2, g1_0, g2_0) 
+    low =  [lowEC50, 1e-5, 1e-5, 1e-5, 1e-5, 1e-5, lowEC50, 1e-5, 1e-5, 1e-5, 0.0, 1, 1]
+    high = [highEC50, 3.0, 3.0, 3.0, 3.0, 3.0, highEC50, 3.0, 3.0, 3.0, 1.0, 60, 60]
 
-    res = bboptimize(residue; SearchRange=collect(zip(low, high)), MaxSteps=60000, TraceMode=:silent)
-    return best_fitness(res), best_candidate(res)
+    results_ode = bboptimize(hillCost; SearchRange=collect(zip(low, high)),
+                                           NumDimensions=length(low),
+                                           TraceMode=:verbose,
+                                           TraceInterval=50,
+                                           MaxSteps=4E4);
+
+    return best_fitness(results_ode), best_candidate(results_ode)
 end
 
-function getODEparams(p::Vector, concentrations::Vector)
-    """ A function to convert the estimated hill parameters back to ODE parameters. """
-    effects = zeros(eltype(p), 4, 8)
+""" A function to convert the estimated hill parameters back to ODE parameters. """
+function getODEparams(p::Array{Float64,1}, concentrations::Array{Float64,1})
+    effects = zeros(eltype(p), 7, 8)
+
     for i in 1:8
+        # [EC50, left, right, steepness]
         effects[1, i] = hill([p[1], p[2], p[3], p[4]], concentrations[i])
         effects[2, i] = hill([p[1], p[5], p[6], p[4]], concentrations[i])
         effects[3, i] = hill([p[7], 0.0,  p[8], p[9]], concentrations[i])
         effects[4, i] = hill([p[7], 0.0, p[10], p[9]], concentrations[i])
+        effects[5, i] = p[11]
+        effects[6, i] = p[12]
+        effects[7, i] = p[13]
     end
     return effects
 end
--- src/ODEmodel.jl
+++ src/ODEmodel.jl
@@ -4,7 +4,7 @@
 
 """ Make the transition matrix. """
 function ODEjac(p, dt::Real, nG1::Int, nG2::Int)
-    # p = [alpha, beta, gamma1, gamma2]
+    # p = [alpha, beta, gamma1, gamma2, nG1, nG2]
     A = diagm( 0 => vec([-ones(nG1)*(p[3]+p[1]); -ones(nG2)*(p[4]+p[2])]),
               -1 => vec([ones(nG1)*p[1];  ones(nG2-1)*p[2]]))
     A[1, end] = 2*p[2]
@@ -21,7 +21,7 @@ function predict(p, g1_0::Real, g2_0::Real, t, nG1::Integer, nG2::Integer)
     # Some assumptions
     @assert t[1] == 0.0
 
-    v = [ones(nG1)*g1_0/nG1; ones(nG2)*g2_0/nG2]
+    v = [ones(nG1)*p[5]*(g1_0+g2_0)/nG1; ones(nG2)*(1.0-p[5])*(g1_0+g2_0)/nG2]
     A = ODEjac(p, t[2], nG1, nG2)
 
     G1 = Vector{eltype(p)}(undef, length(t))
@@ -40,7 +40,7 @@ end
 
 """ Calculates the cost function for a given set of parameters. """
 function cost(p, g1_0, g2_0, g1, g2, nG1::Int, nG2::Int)
-    v = [ones(nG1)*g1_0/nG1; ones(nG2)*g2_0/nG2]
+    v = [ones(nG1)*p[5]*(g1_0+g2_0)/nG1; ones(nG2)*(1.0-p[5])*(g1_0+g2_0)/nG2]
     temp = similar(v)
     A = ODEjac(p, 0.5, nG1, nG2)
 
@@ -58,13 +58,15 @@ end
 
 
 """ Fit the ODE model to data. """
-function ODEoptimizer(i::Int, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array, nG1::Int, nG2::Int)
-    residuals(p) = cost(p, g1_0[i], g2_0[i], g1[:, i], g2[:, i], nG1, nG2)
+function ODEoptimizer(i::Int, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array)
+    residuals(p) = cost(p, g1_0[i], g2_0[i], g1[:, i], g2[:, i], Int(floor(p[6])), Int(floor(p[7])))
     # lower and upper bounds for the parameters
-    bound = collect(zip(zeros(4), ones(4)*nG1*0.1))
+    lower = [0.0, 0.0, 0.0, 0.0, 0.0, 1, 1]
+    upper = [3.0, 3.0, 3.0, 3.0, 1.0, 70, 70]
+    bound = collect(zip(lower, upper))
 
     # global optimization with black box optimization
-    results_ode = bboptimize(residuals; SearchRange=bound, NumDimensions=4, TraceMode=:silent, MaxSteps=50000)
+    results_ode = bboptimize(residuals; SearchRange=bound, NumDimensions=7, TraceMode=:silent, MaxSteps=50000)
 
     return best_fitness(results_ode), best_candidate(results_ode)
 end
@@ -73,10 +75,10 @@ end
 """ Given estimated parameters for each trial, solve the DDE model plot the predicted curve 
     for number of cells in G1, G2, or total, along with their corresponding real data,
     for a longer time which is 2 times of the original time (~195 hours) """
-function ode_plotIt(params::Vector{Float64}, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array, pop, i::Int, title::String, legend::Any, nG1::Int, nG2::Int)
+function ode_plotIt(params::Vector, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::Array, pop, i::Int, title::String, legend::Any)
     t = LinRange(0.0, 95.5, 192)
     t_new = LinRange(0.0, 150, 200)
-    G1, G2 = predict(params, g1_0[i], g2_0[i], t_new, nG1, nG2)
+    G1, G2 = predict(params, g1_0[i], g2_0[i], t_new, Int(floor(params[6])), Int(floor(params[7])))
 
     plot(t_new, G1, label = "G1 est", dpi = 150, xlabel = "time [hours]", ylabel = "# of cells", lw=2.0, alpha = 0.6, color=:green)
     plot!(t, g1[:, i], label = "G1", dpi = 150, markersize = 1.0, color=:darkgreen)
@@ -89,11 +91,11 @@ end
 
 
 """ Plot the data and curves for all concentrations. """
-function ODEplot_all(params_ode, g1_l::Matrix, g2_l::Matrix, g1_0_l::Array, g2_0_l::Array, pop_l, nG1::Int, nG2::Int)
+function ODEplot_all(params_ode, g1_l::Matrix, g2_l::Matrix, g1_0_l::Array, g2_0_l::Array, pop_l)
     # plotting the fitted curves
-    rl = [ode_plotIt(params_ode[:, i], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, i, "", false, nG1, nG2) for i in 1:7]
-    r8 = ode_plotIt(params_ode[:, 8], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, 8, "", :topleft, nG1, nG2)
+    rl = [ode_plotIt(params_ode[:, i], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, i, "", false) for i in 1:7]
+    r8 = ode_plotIt(params_ode[:, 8], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, 8, "", :topleft)
     plot(rl..., r8, layout = (2,4))
-    plot!(size=(1200, 600), layout = (4,2), dpi=200)
+    plot!(size=(900, 400), layout = (4,2), dpi=200)
     ylims!((0.0, 120.0))
 end
