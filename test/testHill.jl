220d48824a3ffa3740d9dc212e82ab84a5d7d11e
aarmey <aarmey@users.noreply.github.com>
2019-12-20 00:06:30 +0000

    Format .jl files

--- test/testHill.jl
+++ test/testHill.jl
@@ -1,19 +1,19 @@
 @testset "Hill tests" begin
-	conc, pop, g2, g1, g1_0, g2_0 = setup_data("lapatinib")
+    conc, pop, g2, g1, g1_0, g2_0 = setup_data("lapatinib")
 
-	t = range(0.0; stop = 95.5, length = 192)
+    t = range(0.0; stop = 95.5, length = 192)
     guess = [100.0, 1.0, 1.0, 1.0, 100, 1.0, 1.0, 1.0, 100, 1.0, 1.0, 100.0, 1.0, 1.0]
     # run the optimization
     fitness, params = optimize_hill(guess, conc, g1, g2, g1_0, g2_0, 50.0, 300.0, 1, 1)
     println(fitness)
 
-	@time optimize_hill(guess, conc, g1, g2, g1_0, g2_0, 50.0, 300.0, 1, 1)
+    @time optimize_hill(guess, conc, g1, g2, g1_0, g2_0, 50.0, 300.0, 1, 1)
 
-	@profile optimize_hill(guess, conc, g1, g2, g1_0, g2_0, 50.0, 300.0, 1, 1)
+    @profile optimize_hill(guess, conc, g1, g2, g1_0, g2_0, 50.0, 300.0, 1, 1)
 
-	Profile.print(noisefloor=5.0)
+    Profile.print(noisefloor = 5.0)
 
     # Check that these at least run
     effects = getODEparams(params, conc)
-	plot_parameters(conc, effects)
+    plot_parameters(conc, effects)
 end
