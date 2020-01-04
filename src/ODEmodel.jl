62bb930720323457669923f6be52355230f3e0a5
farnazmdi <farnazmd95@gmail.com>
2020-01-04 14:15:08 -0800

    bringing the colors back

--- src/ODEmodel.jl
+++ src/ODEmodel.jl
@@ -88,13 +88,15 @@ function ode_plotIt(params::Vector, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::A
         yguidefontsize = 8,
         lw = 2.0,
         alpha = 0.6,
+        color = :green
     )
     plot!(
         t,
         g1[:, i],
         label = "G1",
         dpi = 150,
-        markersize = 1.0
+        markersize = 1.0,
+        color = :darkgreen
     )
     plot!(t_new,
         G2, label = "G2 est",
@@ -102,24 +104,28 @@ function ode_plotIt(params::Vector, g1::Matrix, g2::Matrix, g1_0::Array, g2_0::A
         legendfontsize = 4,
         fg_legend = :transparent,
         lw = 2.0, 
-        alpha = 0.6
+        alpha = 0.6,
+        color = :sienna
     )
     plot!(t
         , g2[:, i],
         label = "G2",
         markersize = 1.0,
+        color = :darkorange
     )
     plot!(t_new
         , G1 .+ G2,
         label = "total est",
         dpi = 150,
         lw = 2.0,
-        alpha = 0.6
+        alpha = 0.6,
+        color = :hotpink
     )
     plot!(t,
         pop[!, i],
         label = "total",
-        markersize = 1.0
+        markersize = 1.0,
+        color = :indigo
     )
     plot!(annotation = [(60, ymax, text(title, 8))])
     ylims!((0.0, ymax))
@@ -129,7 +135,6 @@ end
 """ Plot the data and curves for all concentrations. """
 function ODEplot_all(params_ode, g1_l::Matrix, g2_l::Matrix, g1_0_l::Array, g2_0_l::Array, pop_l, conc::Array{Float64, 1})
     # plotting the fitted curves
-    theme(:mute)
     rl = [ode_plotIt(params_ode[:, i], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, i, string(conc[i], " nM"), false, 80.0) for i = 1:4]
     r2 = [ode_plotIt(params_ode[:, i], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, i, string(conc[i], " nM"), false, 40.0) for i = 5:7]
     r8 = ode_plotIt(params_ode[:, 8], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, 8, string(conc[8], " nM"), :topleft, 40.0)
@@ -142,7 +147,6 @@ function plotPercentage(params::Vector, g1::Matrix, g2::Matrix, g1_0::Array, g2_
     t_new = LinRange(0.0, 120, 200)
     G1, G2 = predict(params, g1_0[i], g2_0[i], t_new, Int(floor(params[6])), Int(floor(params[7])))
 
-    theme(:mute)
     plot(
         t_new,
         100.0*G1 ./ (G1 .+ G2),
@@ -153,12 +157,14 @@ function plotPercentage(params::Vector, g1::Matrix, g2::Matrix, g1_0::Array, g2_
         yguidefontsize = 8,
         lw = 2.0,
         alpha = 0.6,
+        color = :green
     )
     plot!(t
         , 100.0*g1[:, i] ./ pop[!, i],
         label = "G1",
         dpi = 150,
         markersize = 1.0,
+        color =:darkgreen
     )
     plot!(t_new,
         100.0*G2 ./ (G1 .+ G2),
@@ -168,6 +174,7 @@ function plotPercentage(params::Vector, g1::Matrix, g2::Matrix, g1_0::Array, g2_
         fg_legend = :transparent,
         lw = 2.0,
         alpha = 0.6,
+        color = :sienna
     )
     plot!(t,
         100.0*g2[:, i] ./ pop[!,i],
@@ -180,7 +187,6 @@ end
 
 function ODEplot_allPerc(params_ode, g1_l::Matrix, g2_l::Matrix, g1_0_l::Array, g2_0_l::Array, pop_l, conc::Array{Float64, 1})
     # plotting the fitted curves
-    theme(:mute)
     rl = [plotPercentage(params_ode[:, i], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, i, string(conc[i], " nM"), false, 110.0) for i = 1:7]
     r8 = plotPercentage(params_ode[:, 8], g1_l, g2_l, g1_0_l, g2_0_l, pop_l, 8, string(conc[8], " nM"), :topleft, 110.0)
     plot(rl..., r8, layout = (2, 4))
