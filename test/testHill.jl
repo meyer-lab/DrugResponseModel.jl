ed82aedf0b8732f35ec583cf9df82dee5f8ac314
farnazmdi <farnazmd95@gmail.com>
2020-01-12 17:09:46 -0800

    tests changed

--- test/testHill.jl
+++ test/testHill.jl
@@ -1,6 +1,6 @@
 @testset "Hill tests" begin
     conc, pop, g2, g1, g1_0, g2_0 = setup_data("lapatinib")
-    params = [10.0, 1.0, 1.0, 1.0, 1.0, 1.0, 10.0, 1.0, 1.0, 1.0, 1.0, 30, 30]
+    params = [10.0, 1.0, 1.0, 1.0, 1.0, 1.0, 10.0, 1.0, 1.0, 1.0, 1.0, 30, 30, 10, 10]
 
     DrugResponseModel.residHill(params, conc, g1, g2, g1_0, g2_0)
     @time DrugResponseModel.residHill(params, conc, g1, g2, g1_0, g2_0)
--- test/testODE.jl
+++ test/testODE.jl
@@ -3,16 +3,16 @@
 
     t = range(0.0; stop = 95.5, length = 192)
     p = [1.0, 1.1, 0.2, 0.3, 0.5, 20, 20]
-    DrugResponseModel.predict(p, 1.0, 1.0, t, 20, 20)
+    DrugResponseModel.predict(p, 1.0, 1.0, t, 20, 20, 10, 10)
 
     # ODE optimization and estimation of the parameters
     fitness, params_ode = ODEoptimizer(4, g1, g2, g1_0, g2_0)
     @test fitness < 800.0
 
-    @time DrugResponseModel.cost(p, g1_0[1], g2_0[1], g1[:, 1], g2[:, 1], 100, 100)
+    @time DrugResponseModel.cost(p, g1_0[1], g2_0[1], g1[:, 1], g2[:, 1], 100, 100, 20, 20)
 
     for ii = 1:10
-        @profile DrugResponseModel.cost(p, g1_0[1], g2_0[1], g1[:, 1], g2[:, 1], 100, 100)
+        @profile DrugResponseModel.cost(p, g1_0[1], g2_0[1], g1[:, 1], g2[:, 1], 100, 100, 20, 20)
     end
 
     Profile.print(noisefloor = 2.0)
