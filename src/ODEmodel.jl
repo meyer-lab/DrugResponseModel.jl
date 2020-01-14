8cccbf031b11bce28d622b89297800f9964724c3
farnazmdi <farnazmd95@gmail.com>
2020-01-14 10:43:09 -0800

    asserts

--- src/ODEmodel.jl
+++ src/ODEmodel.jl
@@ -6,13 +6,16 @@
 function ODEjac(p::Vector{Float64}, dt::Real, nG1::Int, nG2::Int, nD1::Int, nD2::Int)::Matrix{Float64}
     # p = [alpha, beta, gamma1, gamma2, nG1, nG2, nD1, nD2]
     A = diagm(0 => [-ones(nG1) * (p[3] + p[1]); -ones(nG2) * (p[4] + p[2]); -ones(nD1) * p[3]; -ones(nD2) * p[4]], 
-             -1 => [ones(nG1) * p[1]; ones(nG2 - 1) * p[2] ; 0.0; ones(nD1-1) * p[3] ;0.0; ones(nD2-1) * p[4]])
+             -1 => [ones(nG1) * p[1]; ones(nG2 - 1) * p[2] ; 0.0; ones(nD1-1) * p[3] ;0.0 ; ones(nD2-1) * p[4]])
 
     A[1, nG1+nG2] = 2 * p[2]
-    A[nG1+nG2+1, 1:nG1] .= p[3]
-    A[nG1+nG2+nD1+1, (nG1+1):(nG1+nG2)] .= p[4]
+    A[nG1+nG2+1, 1:nG1] = p[3] * ones(1, nG1)
+    A[nG1+nG2+nD1+1, (nG1+1):(nG1+nG2)] = p[4] * ones(1, nG2)
 
     rmul!(A, dt)
+    @assert A[1:nG1+nG2, nG1+nG2+1:end] == zeros(nG1+nG2, nD1+nD2)
+    @assert A[nG1+nG2+nD1+1, nG1+nG2+nD1] == 0.0
+    @assert A[nG1+nG2+1:nG1+nG2+nD1, nG1+nG2+nD1+1:end]== zeros(nD1, nD2)
     A = LinearAlgebra.exp!(A)
 
     return A
