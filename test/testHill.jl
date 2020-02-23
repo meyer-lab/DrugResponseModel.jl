ccf4deefbc6a673063893d96716127cb03ce2a51
farnazmdi <farnazmd95@gmail.com>
2020-02-23 13:26:42 -0800

    edit 1

--- test/testHill.jl
+++ test/testHill.jl
@@ -1,7 +1,6 @@
 @testset "Hill tests" begin
     conc, pop, g2, g1 = setup_data("lapatinib")
     params = [10.0, 1.0, 1.0, 1.0, 1.0, 1.0, 10.0, 1.0, 1.0, 30, 30, 10, 10]
-    p2 = [10.0, 1.0, 1.0, 1.0, 1.0, 1.0, 10.0, 1.0, 1.0, 30, 30, 0, 10]
 
     DrugResponseModel.residHill(params, conc, g1, g2)
     @time DrugResponseModel.residHill(params, conc, g1, g2)
@@ -13,7 +12,13 @@
 
     # Check that these at least run
     effects = getODEparams(params, conc)
-    eff2 = getODEparams(p2, conc)
     num = numcells(effects[:, 3], g1[1]+g2[1], 100)
-    difcel = diffCell(eff2[:, 1], g1[1]+g2[1], 100)
 end
+
+@testset "Test function appropriately works for different array dimensions" begin
+    conc, pop, g2, g1 = setup_data("lapatinib")
+    p1 = [10.0, 1.0, 1.0, 1.0, 1.0, 1.0, 10.0, 1.0, 1.0, 30, 30, 0, 0]
+    eff1 = getODEparams(p1, conc)
+    difcel = diffCell(eff1[:, 1], g1[1]+g2[1], 100)
+end
+    
