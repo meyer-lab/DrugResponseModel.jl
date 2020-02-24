110f1be13d8114d2da4e312f3920c10d7a056bf9
farnazmdi <farnazmd95@gmail.com>
2020-02-24 10:46:30 -0800

    another test edit

--- test/testDrugAll.jl
+++ test/testDrugAll.jl
@@ -1,7 +1,7 @@
 @testset "Fit All drug at once tests" begin
     concs, populations, g1s, g2s = load()
     p = [0.5*maximum(concs[:, 1]), 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.50, 0.5*maximum(concs[:, 2]), 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.50, 0.5*maximum(concs[:, 3]), 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.50, 0.5*maximum(concs[:, 4]), 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.1, 0.50, 6, 10, 50, 50]
-    effects = getODEparamsAll(p, concs)
+    effects = DrugResponseModel.getODEparamsAll(p, concs)
     @time optimize_hillAll(concs, g1s, g2s; maxstep = 1E2)
     @assert(all(x -> !isnan(x), effects))
     
