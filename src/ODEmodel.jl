935d99a3f8b47af40392c9150f900f91806f7ca0
Aaron Meyer <aaron@asmlab.org>
2020-04-19 11:47:24 -0700

    Remove check.

--- src/ODEmodel.jl
+++ src/ODEmodel.jl
@@ -85,12 +85,8 @@ function predict(p::Vector{T}, g_0, t) where T
         vOut = reshape(vOut[1], (1, :))
     end
 
-    println(size(vOut))
-
     @views G1 = sum(vOut[:, 1:nG1], dims = 2) + sum(vOut[:, (nG1 + nG2 + 1):(nG1 + nG2 + nD1)], dims = 2)
     @views G2 = sum(vOut[:, (nG1 + 1):(nG1 + nG2)], dims = 2) + sum(vOut[:, (nG1 + nG2 + nD1 + 1):(nG1 + nG2 + nD1 + nD2)], dims = 2)
-
-    println(size(G1))
 
     return abs.(vec(G1)), abs.(vec(G2)), vec(vOut[end, :])
 end
