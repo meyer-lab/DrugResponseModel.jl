24987021c850d1106ac8b221e11f761dc1cd1280
farnazmdi <farnazmd95@gmail.com>
2020-06-20 15:59:19 -0700

    two files for recent plots

--- src/allDrugs.jl
+++ src/allDrugs.jl
@@ -8,17 +8,18 @@ function getODEparamsAll(p::Array{Float64, 1}, concentrations::Array{Float64, 2}
     for i = 1:5
         xx = 1.0 ./ (1.0 .+ (p[k] ./ concentrations[:, i]) .^ p[k + 1])
 
-        effects[1, :, i] = p[31] .+ (p[k + 2] - p[31]) .* xx
-        effects[2, :, i] = p[32] .+ (p[k + 3] - p[32]) .* xx
+        effects[1, :, i] = p[36] .+ (p[k + 2] - p[36]) .* xx
+        effects[2, :, i] = p[37] .+ (p[k + 3] - p[37]) .* xx
         effects[3, :, i] = p[k + 4] .* xx
         effects[4, :, i] = p[k + 5] .* xx
-        k += 6
+        effects[5, :, i] .= p[k + 6] #percentage in G1
+        k += 7
     end
-    effects[5, :, :] .= p[33] #percentage in G1
-    effects[6, :, :] .= p[34] #nG1
-    effects[7, :, :] .= p[35] #nG2
-    effects[8, :, :] .= p[36] #nD1
-    effects[9, :, :] .= p[37] #nD2
+
+    effects[6, :, :] .= 1
+    effects[7, :, :] .= 1
+    effects[8, :, :] .= 0
+    effects[9, :, :] .= 0
 
     return effects
 end
@@ -31,20 +32,20 @@ function residHillAll(hillParams::Vector, concentrations::Matrix, g1::Array, g2:
     for j = 1:5
         hill = [
             hillParams[t],
-            hillParams[31],
+            hillParams[36],
             hillParams[t + 2],
             hillParams[t + 1],
-            hillParams[32],
+            hillParams[37],
             hillParams[t + 3],
             hillParams[t + 4],
             hillParams[t + 5],
-            hillParams[33],
-            hillParams[34],
-            hillParams[35],
-            hillParams[36],
-            hillParams[37],
+            hillParams[t + 6],
+            1,
+            1,
+            0,
+            0,
         ]
-        t += 6
+        t += 7
         res += residHill(hill, concentrations[:, j], g1[:, :, j], g2[:, :, j])
     end
 
@@ -57,7 +58,7 @@ function optimize_hillAll(concs::Array{Float64, 2}, g1::Array{Float64, 3}, g2::A
 
     # The parameters used here in order:
     #(:Lap_EC50, :Lap_steepness, :Lap_maxG1ProgRate, :Lap_maxG2ProgRate, :Lap_maxDeathG1Rate, :Lap_maxDeathG2Rate, :Dox_EC50, :Dox_steepness, :Dox_maxG1ProgRate, :Dox_maxG2ProgRate, :Dox_maxDeathG1Rate, :Dox_maxDeathG2Rate, :Gem_EC50, :Gem_steepness, :Gem_maxG1ProgRate, :Gem_maxG2ProgRate, :Gem_maxDeathG1Rate, :Gem_maxDeathG2Rate, :Tax_EC50, :Tax_steepness, :Tax_maxG1ProgRate, :Tax_maxG2ProgRate, :Tax_maxDeathG1Rate, :Tax_maxDeathG2Rate, :pal_EC50, :pal_steepness, :pal_maxG1ProgRate, :pal_maxG2ProgRate, :pal_maxDeathG1Rate, :pal_maxDeathG2Rate, :G1ProgRateControl, :G2ProgRateControl, :percG1, :nG1, :nG2, :nD1, :nD2)
-    lowPiece = [0.01, 3.0, 0.9, 0.0, 0.0]
+    lowPiece = [0.01, 0.05, 0.05, 0.0, 0.0, 0.3]
     low = vcat(
         minimum(concs[:, 1]),
         lowPiece,
@@ -71,13 +72,12 @@ function optimize_hillAll(concs::Array{Float64, 2}, g1::Array{Float64, 3}, g2::A
         lowPiece,
         1e-9,
         1e-9,
-        0.45,
         3,
-        10,
+        3,
         0,
         0,
     )
-    highPiece = [10.0, 7.0, 2.3, 3.0, 3.0]
+    highPiece = [10.0, 5.0, 5.0, 1.0, 1.0, 0.7]
     high = vcat(
         maximum(concs[:, 1]),
         highPiece,
@@ -91,7 +91,6 @@ function optimize_hillAll(concs::Array{Float64, 2}, g1::Array{Float64, 3}, g2::A
         highPiece,
         3.0,
         3.0,
-        0.55,
         50,
         70,
         50,
@@ -174,3 +173,7 @@ function optim_all(concs::Array{Float64, 2}, g1::Array{Float64, 3}, g2::Array{Fl
 
     return Optim.minimizer(results)
 end
+
+# for plotting all the concentrations on top of each other, from average of three reps.
+# function avgAllConcs(g0, g1m, g2m, p)
+    
