5245d021203772797fbfb6391d4947d470758ef8
Aaron Meyer <ameyer@ucla.edu>
2020-11-16 10:44:15 -0800

    More cleanup

--- src/replicates.jl
+++ src/replicates.jl
@@ -13,7 +13,7 @@ function predict_replicates(p1, p2, p3, g0)
         end
     end
 
-    return G1[:, :, 1], G2[:, :, 1], G1[:, :, 2], G2[:, :, 2], G1[:, :, 3], G2[:, :, 3] # all simulation
+    return G1, G2 # all simulation
 end
 
 """ A function to calculate std and mean of ODE parameters for each drug. """
@@ -35,14 +35,14 @@ end
 
 
 """ plots all the three simulated trials. Along with avg and std of data. """
-function plot2(G1_1, G1_2, G1_3, G2_1, G2_2, G2_3, g1s1, g1s2, g1s3, g2s1, g2s2, g2s3, conc, i, j)
+function plot2(G1, G2, g1s1, g1s2, g1s3, g2s1, g2s2, g2s3, conc, i, j)
     time = LinRange(0.0, 95.0, 189)
-    G1 = cat(g1s1, g1s2, g1s3, dims = 4)
-    G2 = cat(g2s1, g2s2, g2s3, dims = 4)
-    meang1 = mean(G1, dims = 4)
-    meang2 = mean(G2, dims = 4)
-    stdg1 = std(G1, dims = 4)
-    stdg2 = std(G2, dims = 4)
+    G1s = cat(g1s1, g1s2, g1s3, dims = 4)
+    G2s = cat(g2s1, g2s2, g2s3, dims = 4)
+    meang1 = mean(G1s, dims = 4)
+    meang2 = mean(G2s, dims = 4)
+    stdg1 = std(G1s, dims = 4)
+    stdg2 = std(G2s, dims = 4)
 
     plot(
         time,
@@ -55,12 +55,12 @@ function plot2(G1_1, G1_2, G1_3, G2_1, G2_2, G2_3, g1s1, g1s2, g1s3, g2s1, g2s2,
         ylabel = "cell number",
         alpha = 0.1,
     )
-    plot!(time, G1_1, label = "G1", color = 6)
-    plot!(time, G1_2, label = "", color = 6)
-    plot!(time, G1_3, label = "", color = 6)
+    plot!(time, G1[:, 1], label = "G1", color = 6)
+    plot!(time, G1[:, 2], label = "", color = 6)
+    plot!(time, G1[:, 3], label = "", color = 6)
     plot!(time, meang2[:, i, j]; ribbon = stdg2[:, i, j], color = 7, label = "", alpha = 0.1)
-    plot!(time, G2_1, label = "G2", color = 7)
-    plot!(time, G2_2, label = "", color = 7)
-    plot!(time, G2_3, label = "", color = 7)
+    plot!(time, G2[:, 1], label = "G2", color = 7)
+    plot!(time, G2[:, 2], label = "", color = 7)
+    plot!(time, G2[:, 3], label = "", color = 7)
     ylims!((0.0, 45))
 end
