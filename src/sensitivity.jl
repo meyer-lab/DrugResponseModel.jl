6d5c353e171cbbfee6c6ee177d5a8ade1c9f2c2c
Farnaz Mohammadi <farnazmd95@gmail.com>
2020-11-18 15:47:00 -0800

    Update sensitivity.jl

--- src/sensitivity.jl
+++ src/sensitivity.jl
@@ -52,5 +52,5 @@ function plotUnitSensitivity(paramRange, result, realParam, i)
         yaxis = :log10,
     )
     plot!([realParam], seriestype = "vline", margin = 0.3cm, legend = :false)
-    ylims!((1E1, 1E5))
+    ylims!((1E2, 1E5))
 end
