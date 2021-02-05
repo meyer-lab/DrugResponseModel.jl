084b8918b73f87f02f179a64012cf98dd4417f57
farnazmdi <farnazmd95@gmail.com>
2021-02-04 18:17:56 -0800

    test

--- test/testODE.jl
+++ test/testODE.jl
@@ -3,7 +3,10 @@
 
     t = LinRange(0.0, 95.5, size(g1, 1))
     p = [1.0, 1.1, 0.2, 0.3, 0.5, 20, 20, 10, 10]
-    DrugResponseModel.predict(p, g1[1, 1] + g2[1, 1], t, g1[:, 1], g2[:, 1])
+    p2 = [100.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.65];
+    DrugResponseModel.ODEjac(p, 1.0)
+    # DrugResponseModel.predict(p, g1[1, 1] + g2[1, 1], t, g1[:, 1], g2[:, 1])
+    
 
-    @time DrugResponseModel.predict(p, g1[1, 1] + g2[1, 1], t, g1[:, 1], g2[:, 1])
+    # @time DrugResponseModel.predict(p, g1[1, 1] + g2[1, 1], t, g1[:, 1], g2[:, 1])
 end
