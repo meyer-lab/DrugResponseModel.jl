0e5c2ff8f56c762245c14379ce168324179af762
Farnaz Mohammadi <farnazmd95@gmail.com>
2021-02-10 09:12:21 -0800

    Update testHill.jl

--- test/testHill.jl
+++ test/testHill.jl
@@ -1,6 +1,6 @@
 @testset "Hill tests" begin
     conc, g2, g1 = load(189, 1)
-    params = [70.0, 1.0, 0.6, 0.03, 1.4, 2.0, 2.0, 0.2, 1.05, 2.0, 0.003, 1.0e-9, 0.06, 0.1, 0.53]
+    params = [70.0, 1.0, 0.6, 0.03, 1.4, 2.0, 2.0, 0.2, 1.05, 2.0, 0.003, 1.0e-9, 0.06, 0.1]
 
     # Check that these at least run
     effects = getODEparams(params, conc)
