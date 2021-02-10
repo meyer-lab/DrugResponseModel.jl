d0435ac7c4c4034c127cab0e170482ddfcf62c77
Farnaz Mohammadi <farnazmd95@gmail.com>
2021-02-10 09:12:03 -0800

    Update testDrugAll.jl

--- test/testDrugAll.jl
+++ test/testDrugAll.jl
@@ -1,6 +1,6 @@
 @testset "Fit All drug at once tests" begin
     concs, populations, g1s, g2s = load(189, 1)
-    p = ones(59)
+    p = ones(54)
     effects = DrugResponseModel.getODEparams(p, concs)
 
     # test the local optimization function
