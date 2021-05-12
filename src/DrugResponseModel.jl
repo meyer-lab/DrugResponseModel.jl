e374ee8f4e41a04c166e83ac0359882965414b06
Farnaz Mohammadi <farnazmd95@gmail.com>
2021-05-11 22:13:29 -0700

    Update testCombination.jl (#455)
    * Update testCombination.jl
    
    I have been paranoid about whether the function that calculates Bliss on cell number is working right or not, so I made this test.
    
    * Update combination.jl
    
    * Update data_model_comb_compare.jmd
    
    * Update figureS4.jl
    
    * Update DrugResponseModel.jl
    
    * Update combination.jmd
    
    * Update figureS4.jl
    
    * Update testCombination.jl
    
    * Update testCombination.jl

--- src/DrugResponseModel.jl
+++ src/DrugResponseModel.jl
@@ -27,7 +27,7 @@ include("sensitivity.jl")
 include("figures/figureS2.jl")
 include("figures/figure3.jl")
 include("figures/figureS3.jl")
-include("figures/figure4.jl")
+# include("figures/figure4.jl")
 include("figures/figureS4.jl")
 
 export setup_data,
