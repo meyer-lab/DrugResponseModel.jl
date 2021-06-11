9ad51072677ddaa052f1b49d15ad1ebe18d44cbb
farnazmdi <farnazmd95@gmail.com>
2021-06-11 09:07:50 -0700

    more and more

--- src/figures/figure4.jl
+++ src/figures/figure4.jl
@@ -456,3 +456,126 @@ function fake_drugs()
     p = plot(p1, p2, layout = (1, 2), size = (900, 400))
     savefig(p, "summ.svg")
 end
+
+function output_combination()
+    concs, popul1, g1s1, g2s1 = load(189, 1)
+    t = LinRange(0.0, 95.0, 189)
+    p = parameters()
+    efcs = getODEparams(p, concs)
+    gem17 = DrugResponseModel.find_gem17(p)
+    efcs[:, 8, 3] = efcs[:, 7, 3]
+    efcs[:, 7, 3] = gem17
+
+    # Bliss on Model
+    LPT_PLB = DrugResponseModel.AllBliss_params(efcs[:, :, 1], efcs[:, :, 5])
+    LPT_GEM = DrugResponseModel.AllBliss_params(efcs[:, :, 1], efcs[:, :, 3])
+    LPT_TAX = DrugResponseModel.AllBliss_params(efcs[:, :, 1], efcs[:, :, 4])
+    GEM_PLB = DrugResponseModel.AllBliss_params(efcs[:, :, 3], efcs[:, :, 5])
+    DOX_GEM = DrugResponseModel.AllBliss_params(efcs[:, :, 2], efcs[:, :, 3])
+
+    ########### Palbociclib 50 nM + lapatinibs [25 nM, 50 nM, 100 nM, 250 nM]
+    palbo50Lap = zeros(3, 189, 4)
+    palbo_alone = zeros(3, 189, 4)
+    lapatinib_alone = zeros(3, 189, 4)
+    for i = 1:4
+        palbo50Lap[1, :, i], palbo50Lap[2, :, i], _ = predict(LPT_PLB[:, i + 3, 5], LPT_PLB[:, 1, 1], t)
+        palbo_alone[1, :, i], palbo_alone[2, :, i], _ = predict(LPT_PLB[:, 1, i + 3], LPT_PLB[:, 1, 1], t)
+        lapatinib_alone[1, :, i], lapatinib_alone[2, :, i], _ = predict(LPT_PLB[:, i + 3, 1], LPT_PLB[:, 1, 1], t)
+    end
+    palbo50Lap[3, :, :] .= palbo50Lap[1, :, :] .+ palbo50Lap[2, :, :]
+    palbo_alone[3, :, :] .= palbo_alone[1, :, :] .+ palbo_alone[2, :, :]
+    lapatinib_alone[3, :, :] .= lapatinib_alone[1, :, :] .+ lapatinib_alone[2, :, :]
+
+
+    ########### Palbociclib 50 nM + gemcitabines [5 nM, 10 nM, 17 nM, 30 nM]
+    palbo50Gem = zeros(3, 189, 4)
+    gemcitabine_alone = zeros(3, 189, 4)
+    for i = 1:4
+        palbo50Gem[1, :, i], palbo50Gem[2, :, i], _ = predict(GEM_PLB[:, i + 4, 5], GEM_PLB[:, 1, 1], t)
+        gemcitabine_alone[1, :, i], gemcitabine_alone[2, :, i], _ = predict(GEM_PLB[:, i + 4, 1], GEM_PLB[:, 1, 1], t)
+    end
+    palbo50Gem[3, :, :] .= palbo50Gem[1, :, :] .+ palbo50Gem[2, :, :]
+    gemcitabine_alone[3, :, :] .= gemcitabine_alone[1, :, :] .+ gemcitabine_alone[2, :, :]
+
+    ########### Gemcitabine 10 nM + lapatinibs [25 nM, 50 nM, 100 nM, 250 nM]
+    Gem10Lap = zeros(3, 189, 4)
+    for i = 1:4
+        Gem10Lap[1, :, i], Gem10Lap[2, :, i], _ = predict(LPT_GEM[:, i + 3, 6], LPT_GEM[:, 1, 1], t)
+    end
+    Gem10Lap[3, :, :] .= Gem10Lap[1, :, :] .+ Gem10Lap[2, :, :]
+
+    ########### Gemcitabine 10 nM + palbociclibs [25 nM, 50 nM, 100 nM, 250 nM]
+    Gem10palbo = zeros(3, 189, 4)
+    for i = 1:4
+        Gem10palbo[1, :, i], Gem10palbo[2, :, i], _ = predict(GEM_PLB[:, 6, i + 4], GEM_PLB[:, 1, 1], t)
+    end
+    Gem10palbo[3, :, :] .= Gem10palbo[1, :, :] .+ Gem10palbo[2, :, :]
+
+    ########### Lap 100 nM + palbociclibs [25 nM, 50 nM, 100 nM, 250 nM]
+    Lap100palbo = zeros(3, 189, 4)
+    for i = 1:4
+        Lap100palbo[1, :, i], Lap100palbo[2, :, i], _ = predict(LPT_PLB[:, 6, i + 3], LPT_PLB[:, 1, 1], t)
+    end
+    Lap100palbo[3, :, :] .= Lap100palbo[1, :, :] .+ Lap100palbo[2, :, :]
+
+    ########### Lap 100 nM + gemcitabines [5 nM, 10 nM, 17 nM, 30 nM]
+    lap100gem = zeros(3, 189, 4)
+    for i = 1:4
+        lap100gem[1, :, i], lap100gem[2, :, i], _ = predict(LPT_GEM[:, 6, i + 4], LPT_GEM[:, 1, 1], t)
+    end
+    lap100gem[3, :, :] .= lap100gem[1, :, :] .+ lap100gem[2, :, :]
+
+    ########## Dox 20 nM + gemcitabines [5 nM, 10 nM, 17 nM, 30 nM]
+    dox20gem = zeros(3, 189, 4)
+    dox_alone = zeros(3, 189)
+    for i = 1:4
+        dox20gem[1, :, i], dox20gem[2, :, i], _ = predict(DOX_GEM[:, 4, i + 4], DOX_GEM[:, 1, 1], t)
+    end
+    dox20gem[1, :, 4], dox20gem[2, :, 4], _ = predict(DOX_GEM[:, 4, 7], DOX_GEM[:, 1, 1], t)
+    dox_alone[1, :], dox_alone[2, :], _ = predict(DOX_GEM[:, 4, 1], DOX_GEM[:, 1, 1], t)
+    dox_alone[3, :] .= dox_alone[1, :] .+ dox_alone[2, :]
+
+    ########### Pax 2 nM + Lapatinib [25 nM, 50 nM, 100 nM, 250 nM]
+    Pax2_lap = zeros(3, 189, 4)
+    pax2_alone = zeros(3, 189)
+    for i = 1:4
+        Pax2_lap[1, :, i], Pax2_lap[2, :, i], _ = predict(LPT_TAX[:, 4, i + 4], LPT_TAX[:, 1, 1], t)
+    end
+    Pax2_lap[3, :, :] .= Pax2_lap[1, :, :] .+ Pax2_lap[2, :, :]
+    pax2_alone[1, :], pax2_alone[2, :], _ = predict(LPT_TAX[:, 1, 4], LPT_TAX[:, 1, 1], t)
+    pax2_alone[3, :] .= pax2_alone[1, :] .+ pax2_alone[2, :]
+
+    # G1
+    df1 = DataFrames.DataFrame(palbo50c=palbo_alone[1, :, 2], palbo50_lpt25=palbo50Lap[1, :, 1], palbo50_lpt50=palbo50Lap[1, :, 2], palbo50_lpt100=palbo50Lap[1, :, 3],palbo50_lpt250=palbo50Lap[1, :, 4], 
+    palbo50=palbo_alone[1, :, 2], palbo50_gem5=palbo50Gem[1, :, 1], palbo50_gem10=palbo50Gem[1, :, 2], palbo50_gem17=palbo50Gem[1, :, 3], palbo50_gem30=palbo50Gem[1, :, 4],
+    gem10c=gemcitabine_alone[1, :, 2], gem10lpt25=Gem10Lap[1, :, 1], gem10lpt50=Gem10Lap[1, :, 2], gem10lpt100=Gem10Lap[1, :, 3], gem10lpt250=Gem10Lap[1, :, 4],
+    gem10=gemcitabine_alone[1, :, 2], gem10_palbo25=Gem10palbo[1, :, 1], gem10_palbo50=Gem10palbo[1, :, 2], gem10_palbo100=Gem10palbo[1, :, 3], gem10_palbo250=Gem10palbo[1, :, 4],
+    lpt100c=lapatinib_alone[1, :, 3], lpt100palbo25=Lap100palbo[1, :, 1], lpt100palbo50=Lap100palbo[1, :, 2], lpt100palbo100=Lap100palbo[1, :, 3], lpt100palbo250=Lap100palbo[1, :, 4],
+    lpt100=lapatinib_alone[1, :, 3], lpt100_gem5=lap100gem[1, :, 1], lpt100_gem10=lap100gem[1, :, 2], lpt100_gem17=lap100gem[1, :, 3], lpt100_gem30=lap100gem[1, :, 4],
+    dox20=dox_alone[1, :], dox20_gem5=dox20gem[1, :, 1], dox20_gem10=dox20gem[1, :, 2], dox20_gem17=dox20gem[1, :, 3], dox20_gem30=dox20gem[1, :, 4],
+    pax2=pax2_alone[1, :], pax2_lpt25=Pax2_lap[1, :, 1], pax2_lpt50=Pax2_lap[1, :, 2], pax2_lpt100=Pax2_lap[1, :, 3], pax2_lpt250=Pax2_lap[1, :, 4])
+
+    # G2
+    df2 = DataFrames.DataFrame(palbo50c=palbo_alone[2, :, 2], palbo50_lpt25=palbo50Lap[2, :, 1], palbo50_lpt50=palbo50Lap[2, :, 2], palbo50_lpt100=palbo50Lap[2, :, 3],palbo50_lpt250=palbo50Lap[2, :, 4], 
+    palbo50=palbo_alone[2, :, 2], palbo50_gem5=palbo50Gem[2, :, 1], palbo50_gem10=palbo50Gem[2, :, 2], palbo50_gem17=palbo50Gem[2, :, 3], palbo50_gem30=palbo50Gem[2, :, 4],
+    gem10c=gemcitabine_alone[2, :, 2], gem10lpt25=Gem10Lap[2, :, 1], gem10lpt50=Gem10Lap[2, :, 2], gem10lpt100=Gem10Lap[2, :, 3], gem10lpt250=Gem10Lap[2, :, 4],
+    gem10=gemcitabine_alone[2, :, 2], gem10_palbo25=Gem10palbo[2, :, 1], gem10_palbo50=Gem10palbo[2, :, 2], gem10_palbo100=Gem10palbo[2, :, 3], gem10_palbo250=Gem10palbo[2, :, 4],
+    lpt100=lapatinib_alone[2, :, 3], lpt100palbo25=Lap100palbo[2, :, 1], lpt100palbo50=Lap100palbo[2, :, 2], lpt100palbo100=Lap100palbo[2, :, 3], lpt100palbo250=Lap100palbo[2, :, 4],
+    lpt100c=lapatinib_alone[2, :, 3], lpt100_gem5=lap100gem[2, :, 1], lpt100_gem10=lap100gem[2, :, 2], lpt100_gem17=lap100gem[2, :, 3], lpt100_gem30=lap100gem[2, :, 4],
+    dox20=dox_alone[2, :], dox20_gem5=dox20gem[2, :, 1], dox20_gem10=dox20gem[2, :, 2], dox20_gem17=dox20gem[2, :, 3], dox20_gem30=dox20gem[2, :, 4],
+    pax2=pax2_alone[2, :], pax2_lpt25=Pax2_lap[2, :, 1], pax2_lpt50=Pax2_lap[2, :, 2], pax2_lpt100=Pax2_lap[2, :, 3], pax2_lpt250=Pax2_lap[2, :, 4])
+
+    # total
+    df3 = DataFrames.DataFrame(palbo50c=palbo_alone[3, :, 2], palbo50_lpt25=palbo50Lap[3, :, 1], palbo50_lpt50=palbo50Lap[3, :, 2], palbo50_lpt100=palbo50Lap[3, :, 3],palbo50_lpt250=palbo50Lap[3, :, 4], 
+    palbo50=palbo_alone[3, :, 2], palbo50_gem5=palbo50Gem[3, :, 1], palbo50_gem10=palbo50Gem[3, :, 2], palbo50_gem17=palbo50Gem[3, :, 3], palbo50_gem30=palbo50Gem[3, :, 4],
+    gem10c=gemcitabine_alone[3, :, 2], gem10lpt25=Gem10Lap[3, :, 1], gem10lpt50=Gem10Lap[3, :, 2], gem10lpt100=Gem10Lap[3, :, 3], gem10lpt250=Gem10Lap[3, :, 4],
+    gem10=gemcitabine_alone[3, :, 2], gem10_palbo25=Gem10palbo[3, :, 1], gem10_palbo50=Gem10palbo[3, :, 2], gem10_palbo100=Gem10palbo[3, :, 3], gem10_palbo250=Gem10palbo[3, :, 4],
+    lpt100c=lapatinib_alone[3, :, 3], lpt100palbo25=Lap100palbo[3, :, 1], lpt100palbo50=Lap100palbo[3, :, 2], lpt100palbo100=Lap100palbo[3, :, 3], lpt100palbo250=Lap100palbo[3, :, 4],
+    lpt100=lapatinib_alone[3, :, 3], lpt100_gem5=lap100gem[3, :, 1], lpt100_gem10=lap100gem[3, :, 2], lpt100_gem17=lap100gem[3, :, 3], lpt100_gem30=lap100gem[3, :, 4],
+    dox20=dox_alone[3, :], dox20_gem5=dox20gem[3, :, 1], dox20_gem10=dox20gem[3, :, 2], dox20_gem17=dox20gem[3, :, 3], dox20_gem30=dox20gem[3, :, 4],
+    pax2=pax2_alone[2, :], pax2_lpt25=Pax2_lap[3, :, 1], pax2_lpt50=Pax2_lap[3, :, 2], pax2_lpt100=Pax2_lap[3, :, 3], pax2_lpt250=Pax2_lap[3, :, 4])
+
+    XLSX.writetable("G1.xlsx", df1)
+    XLSX.writetable("G2.xlsx", df2)
+    XLSX.writetable("Total.xlsx", df3)
+end
