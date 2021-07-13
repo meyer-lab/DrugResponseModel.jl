07f274c0b3a639c9a41b1b0cdba66ea9b65667de
farnazmdi <farnazmd95@gmail.com>
2021-07-12 18:15:58 -0700

    figure3 and S3 updated

--- src/figures/figure3.jl
+++ src/figures/figure3.jl
@@ -97,7 +97,7 @@ function plot_pG1_mean(y1, y2, ymax, Phasename, ylabel, subPlabel, plus)
         y2',
         color = "cyan4",
         xlabel = "drugs",
-        label = "Emax",
+        label = "EC50",
         markerstrokewidth = 0,
         markersize = 8,
         ylabel = ylabel,
@@ -144,20 +144,37 @@ function figure3()
         end
     end
 
-    # deaths
-    deathContG1 = zeros(2, 5)
-    deathEmaxG1 = zeros(2, 5)
-    deathContG1[1, :] .= (efcs[7, 1, :]) ./ (efcs[7, 1, :] .+ efcs[1, 1, :])
-    deathContG1[2, :] .= (1 .- deathContG1[1, :]) .* (efcs[8, 1, :]) ./ (efcs[8, 1, :] .+ efcs[2, 1, :])
-    deathEmaxG1[1, :] .= (efcs[7, 8, :]) ./ (efcs[7, 8, :] .+ efcs[1, 8, :])
-    deathEmaxG1[2, :] .= (1 .- deathEmaxG1[1, :]) .* (efcs[8, 8, :]) ./ (efcs[8, 8, :] .+ efcs[2, 8, :])
+    # params at EC50
+    ec50 = zeros(16, 5)
+    for i = 1:5
+        ec50[:, i] = DrugResponseModel.EC50_params(ps, i)
+    end
+
+    # phase durations
+    # @ control
+    gi = zeros(2, 2, 5)
+    gi[1, 1, :] .= (2 ./ efcs[1, 1, :] .+ 2 ./ efcs[2, 1, :] .+ 2 ./ efcs[3, 1, :] .+ 2 ./ efcs[4, 1, :])
+    gi[2, 1, :] .= (5 ./ efcs[5, 1, :] .+ 5 ./ efcs[6, 1, :] .+ 5 ./ efcs[7, 1, :] .+ 5 ./ efcs[8, 1, :])
+
+    # @ ec50
+    gi[1, 2, :] .= (2 ./ ec50[1, :] .+ 2 ./ ec50[2, :] .+ 2 ./ ec50[3, :] .+ 2 ./ ec50[4, :])
+    gi[2, 2, :] .= (5 ./ ec50[5, :] .+ 5 ./ ec50[6, :] .+ 5 ./ ec50[7, :] .+ 5 ./ ec50[8, :])
+    # cell deaths
+    deathContG1 = zeros(4, 5)
+    deathEC50G1 = zeros(4, 5)
+    deathContG1[1, :] .= (efcs[9, 1, :]) ./ (efcs[9, 1, :] .+ efcs[1, 1, :])
+    deathEC50G1[1, :] .= (ec50[9, :]) ./ (ec50[9, :] .+ ec50[1, :])
+    for i = 2:4
+        deathContG1[i, :] .= (1 .- deathContG1[i - 1, :]) .* (efcs[i + 8, 1, :]) ./ (efcs[i + 8, 1, :] .+ efcs[i, 1, :])
+        deathEC50G1[i, :] .= (1 .- deathEC50G1[i - 1, :]) .* (ec50[i + 8, :]) ./ (ec50[i + 8, :] .+ ec50[i, :])
+    end
     deathContG2 = zeros(4, 5)
-    deathEmaxG2 = zeros(4, 5)
-    deathContG2[1, :] .= (efcs[9, 1, :]) ./ (efcs[9, 1, :] .+ efcs[3, 1, :])
-    deathEmaxG2[1, :] .= (efcs[9, 8, :]) ./ (efcs[9, 8, :] .+ efcs[3, 8, :])
-    for i = 10:12
-        deathContG2[i - 8, :] = (1 .- deathContG2[i - 9, :]) .* (efcs[i, 1, :]) ./ (efcs[i, 1, :] .+ efcs[i - 6, 1, :])
-        deathEmaxG2[i - 9, :] = (1 .- deathEmaxG2[i - 9, :]) .* (efcs[i, 8, :]) ./ (efcs[i, 8, :] .+ efcs[i - 6, 8, :])
+    deathEC50G2 = zeros(4, 5)
+    deathContG2[1, :] .= (efcs[13, 1, :]) ./ (efcs[13, 1, :] .+ efcs[5, 1, :])
+    deathEC50G2[1, :] .= (ec50[13, :]) ./ (ec50[13, :] .+ ec50[5, :])
+    for i = 14:16
+        deathContG2[i - 12, :] = (1 .- deathContG2[i - 13, :]) .* (efcs[i, 1, :]) ./ (efcs[i, 1, :] .+ efcs[i - 8, 1, :])
+        deathEC50G2[i - 12, :] = (1 .- deathEC50G2[i - 13, :]) .* (ec50[i, :]) ./ (ec50[i, :] .+ ec50[i - 8, :])
     end
 
     G1short = zeros(189, 6, 5)
@@ -173,19 +190,15 @@ function figure3()
     g1mshort[:, 2:6, :] .= g1m[:, 4:8, :]
     g2mshort[:, 2:6, :] .= g2m[:, 4:8, :]
 
-    gi = zeros(2, 8, 5)
-    gi[1, :, :] .= (4 ./ efcs[1, :, :] .+ 4 ./ efcs[2, :, :])
-    gi[2, :, :] .= (5 ./ efcs[3, :, :] .+ 5 ./ efcs[4, :, :] .+ 5 ./ efcs[5, :, :] .+ 5 ./ efcs[6, :, :])
-
     p0 = plot(legend = false, grid = false, foreground_color_subplot = :white, top_margin = 1.5cm)
     p1 = plot_fig1(concs[:, 1], G1short[:, :, 1], g1mshort[:, :, 1, 1], "Dynamical Model Fits - Lapatinib", "G1", "B", :PuBu_6)
     p2 = plot_fig1(concs[:, 1], G2short[:, :, 1], g2mshort[:, :, 1, 1], "Dynamical Model Fits - Lapatinib", "S/G2", "C", :PuBu_6)
     p3 = plot_fig1(concs[:, 3], G1short[:, :, 3], g1mshort[:, :, 3, 1], "Dynamical Model Fits - Gemcitabine", "G1", "D", :PuBu_6)
     p4 = plot_fig1(concs[:, 3], G2short[:, :, 3], g2mshort[:, :, 3, 1], "Dynamical Model Fits - Gemcitabine", "S/G2", "E", :PuBu_6)
-    p5 = plot_pG1_mean(gi[1, 1, :]', gi[1, 8, :]', 220.0, "G1 ", "average phase duration [hr]", "F", 10.0)
-    p6 = plot_pG1_mean(gi[2, 1, :]', gi[2, 8, :]', 220.0, "S/G2 ", "average phase duration [hr]", "G", 10.0)
-    p7 = plot_pG1_mean(sum(deathContG1, dims = 1), sum(deathEmaxG1, dims = 1), 1.0, "G1", "death probability", "H", 0.06)
-    p8 = plot_pG1_mean(sum(deathContG2, dims = 1), sum(deathEmaxG2, dims = 1), 1.0, "S/G2", "death probability", "I", 0.06)
+    p5 = plot_pG1_mean(gi[1, 1, :]', gi[1, 2, :]', 50.0, "G1 ", "average phase duration [hr]", "F", 10.0)
+    p6 = plot_pG1_mean(gi[2, 1, :]', gi[2, 2, :]', 50.0, "S/G2 ", "average phase duration [hr]", "G", 10.0)
+    p7 = plot_pG1_mean(sum(deathContG1, dims = 1), sum(deathEC50G1, dims = 1), 1.0, "G1", "death probability", "H", 0.06)
+    p8 = plot_pG1_mean(sum(deathContG2, dims = 1), sum(deathEC50G2, dims = 1), 1.0, "S/G2", "death probability", "I", 0.06)
     p9 = SSE(G1[:, :, :], G2[:, :, :], g1m, g2m, "J")
 
     figure1 = plot(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, size = (2200, 700), layout = (2, 5))
