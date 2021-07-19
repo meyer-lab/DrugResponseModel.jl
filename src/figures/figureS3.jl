9681ad91319cfed1850a7edbe8986d0da91aa165
Farnaz Mohammadi <farnazmd95@gmail.com>
2021-07-19 09:12:22 -0700

    Org figs (#495)
    * reorganizing figures
    
    * oopsi

--- src/figures/figureS3.jl
+++ src/figures/figureS3.jl
@@ -1,59 +1,6 @@
-""" Figure S1 to show the data and model comparison in other 3 drugs. """
+""" To plot the Erlang distributions. """
 
-function plot_pG1(efcs, ymax, Phasename, ylabel, subPlabel, plus)
-
-    x = ["G11", "G12", "G13", "G14", "G21", "G22", "G23", "G24"]
-    y1 = efcs[1:8, 1]
-    y2 = efcs[1:8, 2]
-    scatter(
-        x,
-        y1,
-        color = "red",
-        xlabel = "sub-phase",
-        xrotation = 30,
-        label = "Control",
-        markerstrokewidth = 0,
-        markersize = 8,
-        ylabel = ylabel,
-        titlefont = Plots.font("Helvetica", 12),
-        legendfont = Plots.font("Helvetica", 9),
-        guidefont = Plots.font("Helvetica", 12),
-        xtickfont = Plots.font("Helvetica", 12),
-        ytickfont = Plots.font("Helvetica", 12),
-        bottom_margin = 1.5cm,
-        fg_legend = :transparent,
-        top_margin = 1.5cm,
-        left_margin = 1.25cm,
-        right_margin = 1.25cm,
-        title = "$Phasename effects",
-    )
-    scatter!(
-        x,
-        y2,
-        color = "cyan4",
-        xlabel = "sub-phase",
-        label = "E_EC50",
-        markerstrokewidth = 0,
-        markersize = 8,
-        ylabel = ylabel,
-        titlefont = Plots.font("Helvetica", 12),
-        legendfont = Plots.font("Helvetica", 9),
-        guidefont = Plots.font("Helvetica", 12),
-        xtickfont = Plots.font("Helvetica", 12),
-        ytickfont = Plots.font("Helvetica", 12),
-        bottom_margin = 1.5cm,
-        fg_legend = :transparent,
-        top_margin = 1.5cm,
-        left_margin = 1.25cm,
-        right_margin = 1.25cm,
-        title = "$Phasename effects",
-    )
-    annotate!(-0.5, (ymax + plus), text(subPlabel, :black, :left, Plots.font("Helvetica Bold", 15)))
-    ylims!((-0.1, ymax))
-end
-
-function figureS3()
-
+function plotErlang()
     concs, popul1, g1s1, g2s1 = load(189, 1)
     _, popul2, g1s2, g2s2 = load(189, 2)
     _, popul3, g1s3, g2s3 = load(189, 3)
@@ -63,6 +10,106 @@ function figureS3()
     g2S = cat(g2s1, g2s2, g2s3, dims = 4)
     g1m = mean(g1S, dims = 4) # mean G1
     g2m = mean(g2S, dims = 4) # mean G2
+    basePath = joinpath(dirname(pathof(DrugResponseModel)), "..", "data")
+    path_g = string("/", basePath, "/CCDurations.xlsx")
+    columns, labels = XLSX.readtable(path_g, "CONTROL")
+    columns = hcat(columns)
+
+    # G1 fit
+    G1 = columns[:, 1][1]
+    G1 = convert(Array{Float64, 1}, G1)
+    # Gamma{Float64}(α=6.6035911662388775, θ=2.7246247139426316)
+    d1 = Gamma(6.6, 2.72)
+    lo, hi = quantile.(d1, [0.0001, 0.9999])
+    x1 = range(lo, hi; length = length(G1))
+    pdfG1 = pdf.(d1, x1)
+    coeffG1 = maximum(G1) / maximum(pdfG1)
+
+    # G2 fit
+    Distributions.fit_mle(Gamma, G1)
+    G2 = columns[:, 1][2][1:514]
+    G2 = convert(Array{Float64, 1}, G2)
+    Distributions.fit_mle(Gamma, G2)
+    # Gamma{Float64}(α=22.59370017485455, θ=0.9964125282786628)
+    d2 = Gamma(22.6, 1.0)
+    lo, hi = quantile.(d2, [0.0001, 0.9999])
+    x2 = range(lo, hi; length = length(G2))
+    pdfG2 = pdf.(d2, x2)
+    coeffG2 = maximum(G2) / maximum(pdfG2)
+    # plots
+    fig1 = histogram(
+        G1,
+        normalize = false,
+        xlabel = "time[hr]",
+        ylabel = "abundance",
+        label = "Control data",
+        title = "G1 phase lengths",
+        titlefont = Plots.font("Helvetica", 14),
+        legendfont = Plots.font("Helvetica", 11),
+        guidefont = Plots.font("Helvetica", 14),
+        xtickfont = Plots.font("Helvetica", 14),
+        ytickfont = Plots.font("Helvetica", 14),
+        bottom_margin = 1.5cm,
+        alpha = 0.6,
+        fg_legend = :transparent,
+        top_margin = 1.5cm,
+        left_margin = 1.25cm,
+        right_margin = 1.25cm,
+    )
+    plot!(
+        x1,
+        coeffG1 .* pdfG1,
+        label = "Gamma dist.",
+        titlefont = Plots.font("Helvetica", 14),
+        legendfont = Plots.font("Helvetica", 11),
+        guidefont = Plots.font("Helvetica", 14),
+        xtickfont = Plots.font("Helvetica", 14),
+        ytickfont = Plots.font("Helvetica", 14),
+        bottom_margin = 1.5cm,
+        alpha = 0.6,
+        lw = 3,
+        fg_legend = :transparent,
+        top_margin = 1.5cm,
+        left_margin = 1.25cm,
+        right_margin = 1.25cm,
+    )
+    fig2 = histogram(
+        G2,
+        normalize = false,
+        label = "Control data",
+        xlabel = "time[hr]",
+        ylabel = "abundance",
+        title = "S/G2 phase lengths",
+        titlefont = Plots.font("Helvetica", 14),
+        legendfont = Plots.font("Helvetica", 11),
+        guidefont = Plots.font("Helvetica", 14),
+        xtickfont = Plots.font("Helvetica", 14),
+        ytickfont = Plots.font("Helvetica", 14),
+        bottom_margin = 1.5cm,
+        alpha = 0.6,
+        fg_legend = :transparent,
+        top_margin = 1.5cm,
+        left_margin = 1.25cm,
+        right_margin = 1.25cm,
+    )
+    plot!(
+        x2,
+        coeffG2 .* pdfG2,
+        label = "Gamma dist.",
+        density = false,
+        titlefont = Plots.font("Helvetica", 14),
+        legendfont = Plots.font("Helvetica", 11),
+        guidefont = Plots.font("Helvetica", 14),
+        xtickfont = Plots.font("Helvetica", 14),
+        ytickfont = Plots.font("Helvetica", 14),
+        bottom_margin = 1.5cm,
+        lw = 3,
+        alpha = 0.6,
+        fg_legend = :transparent,
+        top_margin = 1.5cm,
+        left_margin = 1.25cm,
+        right_margin = 1.25cm,
+    )
     ps = parameters()
     efcs = getODEparams(ps, concs)
 
@@ -71,7 +118,6 @@ function figureS3()
         efcs[:, 2, i] = DrugResponseModel.EC50_params(ps, i)
     end
 
-    # ******* model simulations ********
     G1 = zeros(189, 8, 5)
     G2 = zeros(189, 8, 5)
 
@@ -103,43 +149,40 @@ function figureS3()
     G2refshort[:, 2:6, :] .= G2ref[:, 3:7, :]
     g1mshort[:, 2:6, :] .= g1m[:, 3:7, :]
     g2mshort[:, 2:6, :] .= g2m[:, 3:7, :]
-    p1 = DrugResponseModel.plot_fig1(concs[:, 2], G1short[:, :, 2], g1mshort[:, :, 2, 1], "Dynamical Model Fits - Doxorubicin", "G1", "A", :PuBu_6)
-    p2 = DrugResponseModel.plot_fig1(concs[:, 2], G2short[:, :, 2], g2mshort[:, :, 2, 1], "Dynamical Model Fits - Doxorubicin", "S/G2", "B", :PuBu_6)
-    p3 = DrugResponseModel.plot_fig1(concs[:, 4], G1short[:, :, 4], g1mshort[:, :, 4, 1], "Dynamical Model Fits - Paclitaxel", "G1", "C", :PuBu_6)
-    p4 = DrugResponseModel.plot_fig1(concs[:, 4], G2short[:, :, 4], g2mshort[:, :, 4, 1], "Dynamical Model Fits - Paclitaxel", "S/G2", "D", :PuBu_6)
-    p5 = DrugResponseModel.plot_fig1(concs[:, 5], G1short[:, :, 5], g1mshort[:, :, 5, 1], "Dynamical Model Fits - Palbociclib", "G1", "E", :PuBu_6)
-    p6 = DrugResponseModel.plot_fig1(concs[:, 5], G2short[:, :, 5], g2mshort[:, :, 5, 1], "Dynamical Model Fits - Palbociclib", "S/G2", "F", :PuBu_6)
 
     p0 = plot(legend = false, grid = false, foreground_color_subplot = :white, top_margin = 1.5cm)
-    p7 = DrugResponseModel.plot_fig1(concs[:, 1], G1refshort[:, :, 1], g1mshort[:, :, 1, 1], "Expon Model Fits - Lapatinib", "G1", "N", :YlOrBr_6)
-    p8 = DrugResponseModel.plot_fig1(concs[:, 1], G2refshort[:, :, 1], g2mshort[:, :, 1, 1], "Expon Model Fits - Lapatinib", "S/G2", "O", :YlOrBr_6)
-    p9 = DrugResponseModel.plot_fig1(concs[:, 3], G1refshort[:, :, 3], g1mshort[:, :, 3, 1], "Expon Model Fits - Gemcitabine", "G1", "P", :YlOrBr_6)
-    p10 =
-        DrugResponseModel.plot_fig1(concs[:, 3], G2refshort[:, :, 3], g2mshort[:, :, 3, 1], "Expon Model Fits - Gemcitabine", "S/G2", "Q", :YlOrBr_6)
-
-    p11 = DrugResponseModel.plot_pG1(efcs[1:8, 1:2, 1], 2.25, "Lapatinib", "progression rates [1/hr]", "G", 0.35)
-    p12 = DrugResponseModel.plot_pG1(efcs[9:16, 1:2, 1], 0.2, "Lapatinib", "death rates [1/hr]", "H", 0.06)
-    p13 = DrugResponseModel.plot_pG1(efcs[1:8, 1:2, 3], 2.25, "Gemcitabine", "progression rates [1/hr]", "I", 0.35)
-    p14 = DrugResponseModel.plot_pG1(efcs[9:16, 1:2, 3], 0.2, "Gemcitabine", "death rates [1/hr]", "J", 0.06)
-    p15 = DrugResponseModel.plot_pG1(efcs[1:8, 1:2, 5], 2.25, "Paclitaxel", "progression rates [1/hr]", "K", 0.35)
-    p16 = DrugResponseModel.plot_pG1(efcs[9:16, 1:2, 5], 0.2, "Paclitaxel", "death rates [1/hr]", "L", 0.06)
-    figureS3 = plot(p1, p2, p3, p4, p5, p6, p11, p12, p13, p14, p15, p16, p0, p0, p7, p8, p9, p10, size = (2400, 1050), layout = (3, 6))
-    savefig(figureS3, "figureS3.svg")
+    p3 = DrugResponseModel.plot_fig1(concs[:, 1], G1refshort[:, :, 1], g1mshort[:, :, 1, 1], "Expon Model Fits - Lapatinib", "G1", "B", :YlOrBr_6)
+    p4 = DrugResponseModel.plot_fig1(concs[:, 1], G2refshort[:, :, 1], g2mshort[:, :, 1, 1], "Expon Model Fits - Lapatinib", "S/G2", "C", :YlOrBr_6)
+    p5 = DrugResponseModel.plot_fig1(concs[:, 3], G1refshort[:, :, 3], g1mshort[:, :, 3, 1], "Expon Model Fits - Gemcitabine", "G1", "D", :YlOrBr_6)
+    p6 =DrugResponseModel.plot_fig1(concs[:, 3], G2refshort[:, :, 3], g2mshort[:, :, 3, 1], "Expon Model Fits - Gemcitabine", "S/G2", "E", :YlOrBr_6)
+    figs2 = plot(p0, p0, p3, p4, p5, p6, fig1, fig2, layout=(4, 2), size = (800, 1700))
+    savefig(figs2, "figureS3.svg")
 end
 
-""" To plot the baseline Bliss overlayed with experimental data. """
-function plot_figss()
-    concs, popul1, g1s1, g2s1 = load(189, 1)
-    expD = JLD.load("g.jld")["g"]
-    C = output_Bliss_cellnum()
+function output_durations()
+    concs, _, _, _ = load(189, 1)
+    ps = parameters()
+    efcs = getODEparams(ps, concs)
 
-    D = (expD[1, 1:189, [1, 3, 4, 5, 6], [1, 2, 3, 4, 6]] .+ expD[2, 1:189, [1, 3, 4, 5, 6], [1, 2, 3, 4, 6]])
-    p1 = DrugResponseModel.plot_fig1(concs[:, 1], C[:, :, 3, 4], D[:, :, 1], "palbo50 + lpts", "total", "A")
-    p2 = DrugResponseModel.plot_fig1(concs[:, 3], C[:, :, 3, 9], D[:, :, 2], "palbo50 + gems", "total", "B")
-    p3 = DrugResponseModel.plot_fig1(concs[:, 5], C[:, 3, :, 9], D[:, :, 3], "gem10 + palbos", "total", "C")
-    p4 = DrugResponseModel.plot_fig1(concs[:, 1], C[:, :, 3, 2], D[:, :, 4], "gem10 + lpts", "total", "D")
-    p5 = DrugResponseModel.plot_fig1(concs[:, 5], C[:, 4, :, 4], D[:, :, 5], "lpt100 + palbos", "total", "E")
+    gi = zeros(2, 8, 5)
+    gi[1, :, :] .= (2 ./ efcs[1, :, :] .+ 2 ./ efcs[2, :, :] .+ 2 ./ efcs[3, :, :] .+ 2 ./ efcs[4, :, :])
+    gi[2, :, :] .= (5 ./ efcs[5, :, :] .+ 5 ./ efcs[6, :, :] .+ 5 ./ efcs[7, :, :] .+ 5 ./ efcs[8, :, :])
 
-    figureSS = plot(p1, p2, p3, p4, p5, size = (2000, 450), layout = (1, 5))
-    savefig(figureSS, "figureSS.svg")
+    df1 = DataFrames.DataFrame(
+        lapG1 = gi[1, :, 1],
+        doxG1 = gi[1, :, 2],
+        gemG1 = gi[1, :, 3],
+        paxG1 = gi[1, :, 4],
+        palboG1 = gi[1, :, 5],
+        lapG2 = gi[2, :, 1],
+        doxG2 = gi[2, :, 2],
+        gemG2 = gi[2, :, 3],
+        paxG2 = gi[2, :, 4],
+        palboG2 = gi[2, :, 5],
+    )
+    XLSX.writetable("durations.xlsx", df1)
 end
+
+# d1 = DataFrames.DataFrame(controlG1=G1[:, 1, 1], lpt5_G1=G1[:, 2, 1], lpt10_G1=G1[:, 3, 1], lpt25_G1=G1[:, 4, 1], lpt50_G1=G1[:, 5, 1], lpt100_G1=G1[:, 6, 1], lpt250_G1=G1[:, 7, 1], lpt500_G1=G1[:, 8, 1], 
+#                           controlG2=G2[:, 1, 1], lpt5_G2=G2[:, 2, 1], lpt10_G2=G2[:, 3, 1], lpt25_G2=G2[:, 4, 1], lpt50_G2=G2[:, 5, 1], lpt100_G2=G2[:, 6, 1], lpt250_G2=G2[:, 7, 1], lpt500_G2=G2[:, 8, 1], 
+#                           control=G1[:, 1, 1].+G2[:, 1, 1], lpt5=G1[:, 2, 1].+G2[:, 2, 1], lpt10=G1[:, 3, 1].+G2[:, 3, 1], lpt25=G1[:, 4, 1].+G2[:, 4, 1], lpt50=G1[:, 5, 1].+G2[:, 5, 1], lpt100=G1[:, 6, 1].+G2[:, 6, 1], lpt250=G1[:, 7, 1].+G2[:, 7, 1], lpt500=G1[:, 8, 1].+G2[:, 8, 1])
